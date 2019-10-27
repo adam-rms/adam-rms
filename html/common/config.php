9205f340801413509d0e168092d1b5bc3e273a49
James Bithell <james@bithell.com>
2019-10-27 18:38:27 +0000

    Don't require keys - they're optional if using env

--- html/common/config.php
+++ html/common/config.php
@@ -8,7 +8,7 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-require_once (__DIR__ . '/../../keys.php');
+include_once (__DIR__ . '/../../keys.php');
 require_once(__DIR__ . '/../../composer/vendor/autoload.php'); //Composer
 require_once(__DIR__ . '/libs/Auth/main.php');
 require_once(__DIR__ . '/libs/Email/main.php');
