941eab6fbc6077ba5e4f1357ed8cce9915cf46c2
James Bithell <james@bithell.com>
2019-10-27 18:57:16 +0000

    Correct email send issue

--- html/common/config.php
+++ html/common/config.php
@@ -22,9 +22,9 @@ $CONFIG = array(
     //'ERRORS' => ['SENTRY' => getenv('bCMS__SENTRYLOGIN'), "SENTRYPublic" => getenv('bCMS__SENTRYLOGINPUBLIC'), 'URL' => 'https://google.com'],
     'ANALYTICS' => ['TRACKINGID' => getenv('bCMS__GoogleAnalytics')],
     "nextHash" => "sha256", //Hashing algorithm to put new passwords in
-    "PROJECT_FROM_EMAIL" => getenv('iMUN__EMAIL'),
+    "PROJECT_FROM_EMAIL" => getenv('bCMS__EMAIL'),
     "ROOTURL" => "", //Set on a frontend/backend basis
-    "PROJECT_SUPPORT_EMAIL" => getenv('iMUN__SUPPORTEMAIL'),
+    "PROJECT_SUPPORT_EMAIL" => getenv('bCMS__SUPPORTEMAIL'),
     'AWS' => ['UPLOAD' => true, 'KEY' => getenv('iMUN__S3_KEY'), 'SECRET' => getenv('iMUN__S3_SECRET'), 'DEFAULTUPLOADS' => ['BUCKET' => getenv('iMUN__S3_BUCKET'), 'ENDPOINT' =>  getenv('iMUN__S3_ENDPOINT'), 'REGION' => getenv('iMUN__S3_REGION'), 'CDNEndpoint' => getenv('iMUN__S3_CDN')], "FINEUPLOADER" => ["KEY" => getenv('iMUN__S3_KEY'), "SECRET" =>  getenv('iMUN__S3_SECRET')]],
 );
 date_default_timezone_set("UTC");
