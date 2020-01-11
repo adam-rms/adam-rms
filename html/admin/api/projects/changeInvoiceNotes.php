80d19dbd542c8b4836ea7a9969790ad9b1b5c042
James Bithell <james@bithell.com>
2020-01-11 16:15:08 +0000

    Add project invoice notes feature

---
+++ html/admin/api/projects/changeInvoiceNotes.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/../apiHeadSecure.php';
+
+if (!$AUTH->instancePermissionCheck(46) or !isset($_POST['projects_id'])) die("404");
+
+$DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->where("projects.projects_deleted", 0);
+$DBLIB->where("projects.projects_id", $_POST['projects_id']);
+$project = $DBLIB->update("projects", ["projects.projects_invoiceNotes" => $_POST['projects_invoiceNotes']]);
+if (!$project) finish(false);
+
+$bCMS->auditLog("UPDATE-INVOICENOTES", "projects", "Set the invoice notes to ". $_POST['projects_invoiceNotes'], $AUTH->data['users_userid'],null, $_POST['projects_id']);
+finish(true);
