14a7e5cb3d521a025c94ccdee9afccfa9804dd8f
James Bithell <james@bithell.com>
2020-01-19 17:32:30 +0000

    Fix for manufactuer search error

--- html/admin/manufacturers.php
+++ html/admin/manufacturers.php
@@ -9,8 +9,6 @@ else $PAGEDATA['search'] = null;
 if (isset($_GET['page'])) $page = $bCMS->sanitizeString($_GET['page']);
 else $page = 1;
 $DBLIB->pageLimit = 20; //Users per page
-$DBLIB->join("assetTypes", "assets.assetTypes_id=assetTypes.assetTypes_id", "LEFT");
-$DBLIB->join("manufacturers", "manufacturers.manufacturers_id=assetTypes.manufacturers_id", "LEFT");
 $DBLIB->where("(manufacturers.instances_id IS NULL OR manufacturers.instances_id = '" . $AUTH->data['instance']['instances_id'] . "')");
 $DBLIB->orderBy("manufacturers_name", "ASC");
 if (strlen($PAGEDATA['search']) > 0) {
@@ -19,8 +17,15 @@ if (strlen($PAGEDATA['search']) > 0) {
 		manufacturers.manufacturers_name LIKE '%" . $bCMS->sanitizeString($PAGEDATA['search']) . "%' OR
 		manufacturers.manufacturers_website LIKE '%" . $bCMS->sanitizeString($PAGEDATA['search']) . "%' 
     )");
-} else $DBLIB->where("(assets.instances_id = '" . $AUTH->data['instance']['instances_id'] . "' AND assets_deleted = 0)"); //Limit it to where there are assets in the project
-$PAGEDATA['manufacturers'] = $DBLIB->arraybuilder()->paginate('assets', $page, ["DISTINCT manufacturers.*"]);
+	$PAGEDATA['manufacturers'] = $DBLIB->arraybuilder()->paginate('manufacturers', $page, ["manufacturers.*"]);
+} else {
+	//Limit it to where there are assets in the project
+	$DBLIB->join("assetTypes", "assets.assetTypes_id=assetTypes.assetTypes_id", "LEFT");
+	$DBLIB->join("manufacturers", "manufacturers.manufacturers_id=assetTypes.manufacturers_id", "LEFT");
+	$DBLIB->where("(assets.instances_id = '" . $AUTH->data['instance']['instances_id'] . "' AND assets_deleted = 0)");
+	$PAGEDATA['manufacturers'] = $DBLIB->arraybuilder()->paginate('assets', $page, ["DISTINCT manufacturers.*"]);
+}
+
 $PAGEDATA['pagination'] = ["page" => $page, "total" => $DBLIB->totalPages];
 
 echo $TWIG->render('manufacturers.twig', $PAGEDATA);
