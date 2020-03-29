3caf3d0a063a285390aabf9aac1325bea10460a1
James Bithell <james@bithell.com>
2020-03-29 22:07:35 +0100

    Improve logging - closes https://github.com/Jbithell/adam-rms/issues/11

--- AWSLambda/lambda_function.py
+++ AWSLambda/lambda_function.py
@@ -26,21 +26,24 @@ def deletor():
     '''
         Establish a S3 connection
     '''
-    session = boto3.session.Session()
-    
+    s3client = boto3.client('s3')
+
     print("[INFO] S3 Connected")
     
     print("[INFO] Remove files from S3 that are due to be deleted")
-    dbCursor.execute("SELECT * FROM s3files WHERE s3files_meta_deleteOn IS NOT NULL AND s3files_meta_deleteOn <= CURDATE() AND s3files_meta_physicallyStored = 1") #Select everything that needs deleting
+    dbCursor.execute("SELECT s3files_bucket,s3files_path,s3files_filename,s3files_extension,s3files_id FROM s3files WHERE s3files_meta_deleteOn IS NOT NULL AND s3files_meta_deleteOn <= CURRENT_TIMESTAMP() AND s3files_meta_physicallyStored = 1") #Select everything that needs deleting
     listOfFiles = dbCursor.fetchall()
     counter = 0
     for file in listOfFiles:
-        client = session.client('s3')
-        client.delete_object(Bucket=str(file['s3files_bucket']), Key=str(file['s3files_path'])+"/"+str(file['s3files_filename'])+"."+str(file['s3files_extension']))
-        print("[RESULT] Found file that needs deleting & has now been deleted - updating DB")
-        dbCursor.execute("UPDATE s3files SET s3files_meta_physicallyStored = 0 WHERE s3files_id = '" + str(file['s3files_id']) + "'")
-        dbConnection.commit()
-        counter = counter + 1
+        deleteRequest = s3client.delete_object(Bucket=str(file['s3files_bucket']), Key=str(file['s3files_path'])+"/"+str(file['s3files_filename'])+"."+str(file['s3files_extension']))
+        if (True):
+            #Not yet possible to verify if the file has been deleted or not
+            print("[RESULT] Found file that needs deleting (id " + str(file['s3files_id']) + " = " + str(file['s3files_path'])+"/"+str(file['s3files_filename'])+"."+str(file['s3files_extension']) + ") & has now been deleted - updating DB")
+            dbCursor.execute("UPDATE s3files SET s3files_meta_physicallyStored = 0 WHERE s3files_id = '" + str(file['s3files_id']) + "'")
+            dbConnection.commit()
+            counter = counter + 1
+        else:
+            print("[ERROR] Could not delete file with id " + str(file['s3files_id']) + " and path " + str(file['s3files_path'])+"/"+str(file['s3files_filename'])+"."+str(file['s3files_extension']))
     
     return counter
 def lambda_handler(event, context):
