3267793c4a0f779f3a671ff01c81d563fbbf1e7f
James Bithell <james@bithell.com>
2020-04-06 22:20:15 +0100

    Tweak filename for twig caching issue

--- html/admin/instances/new.php
+++ html/admin/instances/new.php
@@ -5,5 +5,5 @@ $PAGEDATA['pageConfig'] = ["TITLE" => "New Business", "BREADCRUMB" => false];
 
 if (!$AUTH->permissionCheck(8)) die("Sorry - you can't access this page");
 
-echo $TWIG->render('instances/new.twig', $PAGEDATA);
+echo $TWIG->render('instances/instances_new.twig', $PAGEDATA);
 ?>
