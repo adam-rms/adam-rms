7804801fc07fe4f439cbeb9d3c9a0a03f57722f1
James Bithell <james@bithell.com>
2020-05-30 14:53:21 +0100

    Add test API endpoint for app

--- html/admin/api/apiHead.php
+++ html/admin/api/apiHead.php
@@ -1,7 +1,10 @@
 <?php
-require_once __DIR__ . '/../common/head.php';
 header('Content-type: application/json');
-
+header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+header("Cache-Control: post-check=0, pre-check=0", false);
+header("Pragma: no-cache");
+header("Access-Control-Allow-Origin: *");
+require_once __DIR__ . '/../common/head.php';
 //To prevent errors showing on the json output
 error_reporting(0);
 ini_set('display_errors', 0);
