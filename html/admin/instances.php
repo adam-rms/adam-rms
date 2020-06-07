e5dd2a914bee91f9cf74169a20f1ddea940d7b3c
James Bithell <james@bithell.com>
2020-06-07 14:29:33 +0100

    Add totals to the instance list

--- html/admin/instances.php
+++ html/admin/instances.php
@@ -25,9 +25,40 @@ if (strlen($PAGEDATA['search']) > 0) {
 $instances = $DBLIB->arraybuilder()->paginate('instances', $page, ["instances.*"]);
 $PAGEDATA['pagination'] = ["page" => $page, "total" => $DBLIB->totalPages];
 $PAGEDATA['instances'] = [];
+$PAGEDATA['totals'] = ["assets" => ["VALUE" => 0.0,"MASS" => 0.0, "COUNT" => 0], "STORAGEUSED" => 0, "STORAGEALLOWED" => 0];
 foreach ($instances as $instance) {
+	//Inventory
+	$DBLIB->where("assets.instances_id", $instance['instances_id']);
+	$DBLIB->orderBy("assets_inserted", "ASC");
+	$DBLIB->where("assets_deleted", 0);
+	$DBLIB->join("assetTypes", "assets.assetTypes_id=assetTypes.assetTypes_id", "LEFT");
+	$assets = $DBLIB->get("assets", null, ["assets_inserted", "assetTypes_value", "assetTypes_mass"]);
+	$instance['assets'] = ["VALUE" => 0.0,"MASS" => 0.0, "COUNT" => 0];
+	foreach ($assets as $asset) {
+		$instance['assets']['VALUE'] += $asset['assetTypes_value'];
+		$instance['assets']['MASS'] += $asset['assetTypes_mass'];
+		$instance['assets']['COUNT'] += 1;
+		$PAGEDATA['totals']['assets']['VALUE'] += $asset['assetTypes_value'];
+		$PAGEDATA['totals']['assets']['MASS'] += $asset['assetTypes_mass'];
+		$PAGEDATA['totals']['assets']['COUNT'] += 1;
+	}
+
+	//Storage
+	$DBLIB->where("s3files.instances_id", $instance['instances_id']);
+	$DBLIB->where("(s3files_meta_deleteOn IS NULL)");
+	$DBLIB->where("s3files_meta_physicallyStored", 1);
+	$instance['STORAGEUSED'] = $DBLIB->getValue("s3files", "SUM(s3files_meta_size)");
+	$PAGEDATA['totals']['STORAGEUSED'] += $instance['STORAGEUSED'];
+	$PAGEDATA['totals']['STORAGEALLOWED'] += $instance['instances_storageLimit'];
+	//USERS
+	$DBLIB->join("userInstances", "users.users_userid=userInstances.users_userid","LEFT");
+	$DBLIB->join("instancePositions", "userInstances.instancePositions_id=instancePositions.instancePositions_id","LEFT");
+	$DBLIB->where("instances_id",  $instance['instances_id']);
+	$DBLIB->where("userInstances.userInstances_deleted",  0);
+	$instance['USERS'] = $DBLIB->getValue("users","COUNT(*)");
+
+
 	$PAGEDATA['instances'][] = $instance;
 }
-
 echo $TWIG->render('instances.twig', $PAGEDATA);
 ?>
