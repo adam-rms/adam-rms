e3592ce0223b4a287b4f5abb7e1377cd6a2b6b38
James Bithell <james@bithell.com>
2020-06-08 20:21:02 +0100

    Remove duplicate

--- html/admin/api/assets/editAssetType.php
+++ html/admin/api/assets/editAssetType.php
@@ -14,9 +14,6 @@ if (!$AUTH->permissionCheck(19)) {
     $DBLIB->where("instances_id",$AUTH->data['instance']["instances_id"]);
 }
 $DBLIB->where("assetTypes_id",$array['assetTypes_id']);
-if ($array['assetTypes_mass'] == "") $array['assetTypes_mass'] = null; //This is odd but seems to fix an error
-if ($array['assetTypes_dayRate'] == "") $array['assetTypes_dayRate'] = null; //This is odd but seems to fix an error
-if ($array['assetTypes_weekRate'] == "") $array['assetTypes_weekRate'] = null; //This is odd but seems to fix an error
 
 $result = $DBLIB->update("assetTypes", array_intersect_key( $array, array_flip( ['assetTypes_name','assetCategories_id','assetTypes_productLink','manufacturers_id','assetTypes_description','assetTypes_definableFields','assetTypes_mass','assetTypes_inserted',"assetTypes_dayRate","assetTypes_weekRate","assetTypes_value"] ) ));
 if (!$result) finish(false, ["code" => "UPDATE-FAIL", "message"=> "Could not update asset type"]);
