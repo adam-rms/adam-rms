08905c97226b0e011f326aa6acb3fb91af8e54a4
James Bithell <james@bithell.com>
2020-06-08 19:02:55 +0100

    Add API endpoint for https://github.com/Jbithell/adam-rms/issues/6

---
+++ html/admin/api/instances/editInstance.php
@@ -0,0 +1,18 @@
+<?php
+require_once __DIR__ . '/../apiHeadSecure.php';
+
+if (!$AUTH->instancePermissionCheck(83)) die("Sorry - you can't access this page");
+$array = [];
+foreach ($_POST['formData'] as $item) {
+    if ($item['value'] == '') $item['value'] = null;
+    $array[$item['name']] = $item['value'];
+}
+if (strlen($array['assets_id']) < 1) finish(false, ["code" => "PARAM-ERROR", "message"=> "No data for action"]);
+
+$DBLIB->where("instances_id",$AUTH->data['instance']["instances_id"]);
+$result = $DBLIB->update("instances", array_intersect_key( $array, array_flip( ["instances_name","instances_address","instances_phone","instances_email","instances_website","instances_weekStartDates","instances_logo","instances_emailHeader","instances_termsAndPayment"] ) ));
+if (!$result) finish(false, ["code" => "UPDATE-FAIL", "message"=> "Could not update instance"]);
+else {
+    $bCMS->auditLog("EDIT-INSTANCE", "instances", json_encode($array), $AUTH->data['users_userid'],null, $AUTH->data['instance']["instances_id"]);
+    finish(true);
+}
