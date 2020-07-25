432eb5fbedfc938f95c26ea551bb804cc2c2e2ac
James Bithell <james@bithell.com>
2020-07-25 15:49:59 +0100

    Closes https://github.com/Jbithell/adam-rms/issues/29

---
+++ html/admin/api/categories/edit.php
@@ -0,0 +1,19 @@
+<?php
+require_once __DIR__ . '/../apiHeadSecure.php';
+
+if (!$AUTH->instancePermissionCheck(91)) die("404");
+
+$array = [];
+foreach ($_POST['formData'] as $item) {
+    $array[$item['name']] = $item['value'];
+}
+if (strlen($array['assetCategories_id']) <1) finish(false, ["code" => "PARAM-ERROR", "message"=> "No data for action"]);
+
+$DBLIB->where("instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->where("assetCategories_deleted", 0);
+$DBLIB->where("assetCategories_id", $array['assetCategories_id']);
+$category = $DBLIB->update("assetCategories", $array);
+if (!$category) finish(false);
+
+$bCMS->auditLog("EDIT", "assetCategories", json_encode($array), $AUTH->data['users_userid']);
+finish(true);
--- html/admin/asset.php
+++ html/admin/asset.php
@@ -64,6 +64,8 @@ $DBLIB->orderBy("manufacturers_name", "ASC");
 $PAGEDATA['manufacturers'] = $DBLIB->get('manufacturers', null, ["manufacturers.manufacturers_id", "manufacturers.manufacturers_name"]);
 
 $DBLIB->orderBy("assetCategories_rank", "ASC");
+$DBLIB->where("(instances_id IS NULL OR instances_id = '" . $AUTH->data['instance']["instances_id"] . "')");
+$DBLIB->where("assetCategories_deleted",0);
 $DBLIB->join("assetCategoriesGroups", "assetCategoriesGroups.assetCategoriesGroups_id=assetCategories.assetCategoriesGroups_id", "LEFT");
 $PAGEDATA['categories'] = $DBLIB->get('assetCategories');
 
