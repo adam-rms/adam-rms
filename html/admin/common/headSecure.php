07ea9910a05f00fcfa5ac24f9ab76f17fae0ae74
James Bithell <james@bithell.com>
2020-07-26 16:19:23 +0100

    Closes https://github.com/Jbithell/adam-rms/issues/35

--- html/admin/common/headSecure.php
+++ html/admin/common/headSecure.php
@@ -13,66 +13,65 @@ if (!$CONFIG['DEV']) {
         $scope->setUser(['username' => $GLOBALS['AUTH']->data['users_username'],"id"=> $GLOBALS['AUTH']->data['users_userid']]);
         if ($GLOBALS['AUTH']->data['instance']) $scope->setExtra('instances_id', $AUTH->data['instance']['instances_id']);
     });
+} elseif (!$AUTH->permissionCheck(17) and !$PAGEDATA['USERDATA']['viewSiteAs']) {
+    die("Sorry - you can't use this development version of the site - please visit adam-rms.com");
 }
 
-$DBLIB->where("((SELECT COUNT(*) FROM assets WHERE assets.assetTypes_id=assetTypes.assetTypes_id AND assets_deleted = '0' AND assets.instances_id = '" . $AUTH->data['instance']['instances_id'] . "') > 0)");
-$assetCategories = $DBLIB->getvalue("assetTypes", "DISTINCT assetCategories_id", null);
-if ($assetCategories) {
-    $DBLIB->orderBy("assetCategoriesGroups.assetCategoriesGroups_order", "ASC");
-    $DBLIB->orderBy("assetCategories.assetCategories_rank", "ASC");
-    $DBLIB->where("(assetCategories_id IN (" . implode(",", $assetCategories) . "))");
-    $DBLIB->join("assetCategoriesGroups", "assetCategoriesGroups.assetCategoriesGroups_id=assetCategories.assetCategoriesGroups_id", "LEFT");
-    $PAGEDATA['assetCategories'] = $DBLIB->get("assetCategories", null, ["assetCategories.assetCategories_name", "assetCategories.assetCategories_id", "assetCategories.assetCategories_fontAwesome", "assetCategoriesGroups_name", "assetCategoriesGroups_fontAwesome"]);
-} else $PAGEDATA['assetCategories'] = [];
+if ($AUTH->data['instance']) {
+    $DBLIB->where("((SELECT COUNT(*) FROM assets WHERE assets.assetTypes_id=assetTypes.assetTypes_id AND assets_deleted = '0' AND assets.instances_id = '" . $AUTH->data['instance']['instances_id'] . "') > 0)");
+    $assetCategories = $DBLIB->getvalue("assetTypes", "DISTINCT assetCategories_id", null);
+    if ($assetCategories) {
+        $DBLIB->orderBy("assetCategoriesGroups.assetCategoriesGroups_order", "ASC");
+        $DBLIB->orderBy("assetCategories.assetCategories_rank", "ASC");
+        $DBLIB->where("(assetCategories_id IN (" . implode(",", $assetCategories) . "))");
+        $DBLIB->join("assetCategoriesGroups", "assetCategoriesGroups.assetCategoriesGroups_id=assetCategories.assetCategoriesGroups_id", "LEFT");
+        $PAGEDATA['assetCategories'] = $DBLIB->get("assetCategories", null, ["assetCategories.assetCategories_name", "assetCategories.assetCategories_id", "assetCategories.assetCategories_fontAwesome", "assetCategoriesGroups_name", "assetCategoriesGroups_fontAwesome"]);
+    } else $PAGEDATA['assetCategories'] = [];
 
-$DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
-$DBLIB->where("projects.projects_deleted", 0);
-$DBLIB->where("projects.projects_archived", 0);
-$DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
-$DBLIB->orderBy("projects.projects_dates_deliver_start", "ASC");
-$DBLIB->orderBy("projects.projects_name", "ASC");
-$DBLIB->orderBy("projects.projects_created", "ASC");
-$PAGEDATA['projects'] = $DBLIB->get("projects", null, ["projects_id", "projects_archived", "projects_name", "clients_name", "projects_dates_deliver_start", "projects_dates_deliver_end","projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_manager"]);
-$PAGEDATA['thisProject'] = false;
-if ($AUTH->data['users_selectedProjectID'] != null) {
-    foreach ($PAGEDATA['projects'] as $project) {
-        if ($project['projects_id'] == $AUTH->data['users_selectedProjectID'] ) {
-            $PAGEDATA['thisProject'] = $project;
-            break;
+    $DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
+    $DBLIB->where("projects.projects_deleted", 0);
+    $DBLIB->where("projects.projects_archived", 0);
+    $DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
+    $DBLIB->orderBy("projects.projects_dates_deliver_start", "ASC");
+    $DBLIB->orderBy("projects.projects_name", "ASC");
+    $DBLIB->orderBy("projects.projects_created", "ASC");
+    $PAGEDATA['projects'] = $DBLIB->get("projects", null, ["projects_id", "projects_archived", "projects_name", "clients_name", "projects_dates_deliver_start", "projects_dates_deliver_end","projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_manager"]);
+    $PAGEDATA['thisProject'] = false;
+    if ($AUTH->data['users_selectedProjectID'] != null) {
+        foreach ($PAGEDATA['projects'] as $project) {
+            if ($project['projects_id'] == $AUTH->data['users_selectedProjectID'] ) {
+                $PAGEDATA['thisProject'] = $project;
+                break;
+            }
+        }
+        if (!$PAGEDATA['thisProject']) {
+            //They're browsing with a project selected from another instance so we need to go grab it
+            $DBLIB->where("projects.projects_id", $AUTH->data['users_selectedProjectID']);
+            $DBLIB->where("projects.instances_id IN (" . implode(",", $AUTH->data['instance_ids']) . ")"); //Duplicated elsewhere
+            $DBLIB->where("projects.projects_deleted", 0);
+            $DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
+            $DBLIB->join("instances", "projects.instances_id=instances.instances_id", "LEFT");
+            $DBLIB->orderBy("projects.projects_dates_deliver_start", "ASC");
+            $DBLIB->orderBy("projects.projects_name", "ASC");
+            $DBLIB->orderBy("projects.projects_created", "ASC");
+            $PAGEDATA['thisProject'] = $DBLIB->getone("projects", null, ["projects_id", "projects_archived", "projects_name", "clients_name", "projects_dates_deliver_start", "projects_dates_deliver_end","projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_manager", "instances.instances_name"]);
         }
     }
-    if (!$PAGEDATA['thisProject']) {
-        //They're browsing with a project selected from another instance so we need to go grab it
-        $DBLIB->where("projects.projects_id", $AUTH->data['users_selectedProjectID']);
-        $DBLIB->where("projects.instances_id IN (" . implode(",", $AUTH->data['instance_ids']) . ")"); //Duplicated elsewhere
-        $DBLIB->where("projects.projects_deleted", 0);
-        $DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
-        $DBLIB->join("instances", "projects.instances_id=instances.instances_id", "LEFT");
-        $DBLIB->orderBy("projects.projects_dates_deliver_start", "ASC");
-        $DBLIB->orderBy("projects.projects_name", "ASC");
-        $DBLIB->orderBy("projects.projects_created", "ASC");
-        $PAGEDATA['thisProject'] = $DBLIB->getone("projects", null, ["projects_id", "projects_archived", "projects_name", "clients_name", "projects_dates_deliver_start", "projects_dates_deliver_end","projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_manager", "instances.instances_name"]);
-    }
+
+    if ($AUTH->data['instance']['instances_weekStartDates'] != null) {
+        $dates = explode("\n", $AUTH->data['instance']['instances_weekStartDates']);
+        $AUTH->data['instance']['weekStartDates'] = [];
+        foreach ($dates as $date) {
+            array_push($AUTH->data['instance']['weekStartDates'], strtotime($date)*1000);
+        }
+        unset($dates);
+        sort($AUTH->data['instance']['weekStartDates']);
+    } else $AUTH->data['instance']['weekStartDates'] = false;
 }
-
-if ($AUTH->data['instance']['instances_weekStartDates'] != null) {
-    $dates = explode("\n", $AUTH->data['instance']['instances_weekStartDates']);
-    $AUTH->data['instance']['weekStartDates'] = [];
-    foreach ($dates as $date) {
-        array_push($AUTH->data['instance']['weekStartDates'], strtotime($date)*1000);
-    }
-    unset($dates);
-    sort($AUTH->data['instance']['weekStartDates']);
-} else $AUTH->data['instance']['weekStartDates'] = false;
-
 
 $PAGEDATA['USERDATA'] = $GLOBALS['AUTH']->data;
 $PAGEDATA['USERDATA']['users_email_md5'] = md5($PAGEDATA['USERDATA']['users_email']);
 
-
-if ($CONFIG['DEV'] and !$AUTH->permissionCheck(17) and !$PAGEDATA['USERDATA']['viewSiteAs']) {
-    die("Sorry - you can't use this development version of the site - please visit adam-rms.com");
-}
 
 $USERDATA = $PAGEDATA['USERDATA']; //TODO - remove as depreciated
 ?>
