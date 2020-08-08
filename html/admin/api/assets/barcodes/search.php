d4ba051536c6f4b162400a8782f6aa3bf8157eab
James Bithell <james@bithell.com>
2020-08-08 17:11:01 +0100

    Fix location scan bug

--- html/admin/api/assets/barcodes/search.php
+++ html/admin/api/assets/barcodes/search.php
@@ -8,6 +8,7 @@ $locationBarcode = $DBLIB->getone("locationsBarcodes",["locations_id","locations
 if ($locationBarcode) {
     $DBLIB->where("locations.instances_id", $AUTH->data['instance']['instances_id']);
     $DBLIB->where("locations.locations_deleted", 0);
+    $DBLIB->where("locations.locations_id",$locationBarcode['locations_id']);
     $location = $DBLIB->getOne("locations", ["locations_id","locations_name"]);
     if ($location) {
         $location['barcode'] = $locationBarcode;
@@ -20,7 +21,7 @@ $DBLIB->where("assetsBarcodes_value", $_POST['text']);
 $DBLIB->where("assetsBarcodes_type", $_POST['type']);
 $DBLIB->where("assetsBarcodes_deleted",0);
 $barcode = $DBLIB->getone("assetsBarcodes",["assets_id","assetsBarcodes_id"]);
-if (!$barcode) finish(true, null, ["asset" => false, "barcode" => false]);
+if (!$barcode) finish(true, null, ["asset" => false, "barcode" => false,"location" => ($location ? $location : false)]);
 
 $scan = [
     "assetsBarcodes_id" => $barcode['assetsBarcodes_id'],
@@ -32,7 +33,7 @@ $scan = [
 ];
 $DBLIB->insert("assetsBarcodesScans",$scan);
 
-if ($barcode['assets_id'] == null) finish(true, null, ["asset" => false, "barcode" => $barcode['assetsBarcodes_id']]);
+if ($barcode['assets_id'] == null) finish(true, null, ["asset" => false, "barcode" => $barcode['assetsBarcodes_id'],"location" => ($location ? $location : false)]);
 
 $DBLIB->where("(assetTypes.instances_id IS NULL OR assetTypes.instances_id IN (" . implode(",",$AUTH->data['instance_ids']) . "))");
 $DBLIB->join("assetTypes","assets.assetTypes_id=assetTypes.assetTypes_id", "LEFT");
@@ -41,7 +42,7 @@ $DBLIB->join("assetCategories", "assetCategories.assetCategories_id=assetTypes.a
 $DBLIB->join("assetCategoriesGroups", "assetCategoriesGroups.assetCategoriesGroups_id=assetCategories.assetCategoriesGroups_id", "LEFT");
 $DBLIB->where("assets.assets_id",$barcode['assets_id']);
 $asset = $DBLIB->getone("assets", ["assets.assets_id", "assets.assets_tag", "assetTypes.assetTypes_name", "assetTypes.assetTypes_id", "assetCategories.assetCategories_name", "assetCategoriesGroups.assetCategoriesGroups_name", "manufacturers.manufacturers_name"]);
-if (!$asset) finish(true, null, ["asset" => false, "barcode" => $barcode['assetsBarcodes_id']]);
+if (!$asset) finish(true, null, ["asset" => false, "barcode" => $barcode['assetsBarcodes_id'],"location" => ($location ? $location : false)]);
 
 //Format asset tag
 if ($asset['assets_tag'] == null) $asset['tag']= '';
