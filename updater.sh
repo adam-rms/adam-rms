f9c0d11b5e1e377b8a8b2b9ee2b9865526776925
James Bithell <james@bithell.com>
2020-08-11 16:59:39 +0100

    Add git pull to updater

--- updater.sh
+++ updater.sh
@@ -1,3 +1,4 @@
+git pull
 docker build -t adamrms .
 docker stop adamrms-container && docker rm adamrms-container
 docker run -d -p 80:80 --restart=always --memory=800m --name adamrms-container adamrms
