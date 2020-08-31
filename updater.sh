c407827e6ec5c0f3a58a43e52285d99ccfefb5e0
James Bithell <james@bithell.com>
2020-08-31 14:33:19 +0100

    Tweak updater to limit memory and add swap

--- updater.sh
+++ updater.sh
@@ -1,4 +1,4 @@
 git pull
 docker build -t adamrms .
 docker stop adamrms-container && docker rm adamrms-container
-docker run -d -p 80:80 --restart=always --memory=800m --name adamrms-container adamrms
+docker run -d -p 80:80 --restart=always --memory=400m --memory-swap="5g" --name adamrms-container adamrms
