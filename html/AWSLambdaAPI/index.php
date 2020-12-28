5b03bf29b705c249f54ae901c1e1faeaa2cdbb77
James Bithell <james@bithell.com>
2020-12-28 16:39:07 +0000

    Build basic framework for AWS elements

--- html/AWSLambdaAPI/index.php
+++ html/AWSLambdaAPI/index.php
@@ -2,16 +2,26 @@
 $lambdaContext = json_decode($_SERVER['LAMBDA_INVOCATION_CONTEXT'], true);
 $requestContext = json_decode($_SERVER['LAMBDA_REQUEST_CONTEXT'], true);
 if ($requestContext['path']) {
-    $path = str_replace("/prod","admin", $requestContext['path']);
-    if (file_exists($path)) {
-        include $path;
+    $path = str_replace("/prod","/", $requestContext['path']);
+} else $path = parse_url($_SERVER["REQUEST_URI"], PHP_URL_PATH);
+$endpoints = [
+    [
+        "url" => "/info",
+        "file" => "endpoints/misc/info.php",
+        "secure" => false
+    ]
+];
+
+foreach ($endpoints as $endpoint) {
+    if ($path == $endpoint['url']) {
+        if ($endpoint['secure']) $SECURE = true;
+        require_once 'lambdaHead.php';
+        require $endpoint['file'];
         exit;
-    } elseif (file_exists($path . "/index.php")) {
-        include $path . "/index.php";
-        exit;
-    } elseif (file_exists($path . "index.php")) {
-        include $path . "index.php";
-        exit;
-    } else die ("4042");
-} else die("404");
+    }
+}
+
+http_response_code(404);
+include "404.html";
+exit;
 ?>
--- html/AWSLambdaAPI/lambdaHead.php
+++ html/AWSLambdaAPI/lambdaHead.php
@@ -1,4 +1,5 @@
 <?php
+require_once __DIR__ . '/../common/libs/Auth/main.php';
 header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
 header("Cache-Control: post-check=0, pre-check=0", false);
 header("Pragma: no-cache");
@@ -95,20 +96,12 @@ class assetAssignmentSelector
     }
 }
 
-
-
+$GLOBALS['AUTH'] = new bID;
 if (isset($SECURE) and $SECURE) {
     if (!$GLOBALS['AUTH']->login) {
         if ($CONFIG['DEV']) finish(false,["message"=>"AUTH FAIL - " . $GLOBALS['AUTH']->debug]);
         else finish(false,["code" => "AUTH", "message"=>"AUTH FAIL"]);
     }
-    if (!$CONFIG['DEV']) {
-        Sentry\configureScope(function (Sentry\State\Scope $scope): void {
-            $scope->setUser(['username' => $GLOBALS['AUTH']->data['users_username'],"id"=> $GLOBALS['AUTH']->data['users_userid']]);
-            if ($GLOBALS['AUTH']->data['instance']) $scope->setExtra('instances_id', $AUTH->data['instance']['instances_id']);
-        });
-    } elseif (!$AUTH->permissionCheck(17) and !$GLOBALS['AUTH']->data['viewSiteAs']) finish(false,["message"=>"Can't use dev site"]);
-
     $PAGEDATA['USERDATA'] = $GLOBALS['AUTH']->data;
     $PAGEDATA['USERDATA']['users_email_md5'] = md5($PAGEDATA['USERDATA']['users_email']);
 }
