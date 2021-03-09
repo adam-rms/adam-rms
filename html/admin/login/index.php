1a90d1f033d5c70819726553580f4a879989b06c
James Bithell <james@bithell.com>
2021-03-09 16:16:30 +0000

    Stop logout stopping you log back in again

--- html/admin/login/index.php
+++ html/admin/login/index.php
@@ -7,7 +7,11 @@ if (isset($_SESSION['return'])) {
 	$PAGEDATA['return'] = $_SESSION['return'];
 } else $PAGEDATA['return'] =$CONFIG['ROOTURL'];
 
-if (isset($_GET['logout'])) $AUTH->logout();
+if (isset($_GET['logout'])) {
+	$AUTH->logout();
+	header("Location: " . $CONFIG['ROOTURL'] . "/");
+	die('<meta http-equiv="refresh" content="0; url="' . $CONFIG['ROOTURL'] . "/" . '" />');
+}
 elseif ($GLOBALS['AUTH']->login) {
 	//If they're logged in, take them back to root - this fixes a loop redirect issue
 	header("Location: " . $CONFIG['ROOTURL'] . "/");
