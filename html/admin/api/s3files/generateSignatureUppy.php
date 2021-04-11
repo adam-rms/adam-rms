885048b2f4c2a00df18b4fa5bf8bedf0d1d2ea16
James Bithell <james@bithell.com>
2021-04-11 19:03:36 +0100

    List allowed methods https://stackoverflow.com/a/53262825/3088158

--- html/admin/api/s3files/generateSignatureUppy.php
+++ html/admin/api/s3files/generateSignatureUppy.php
@@ -4,8 +4,8 @@ $body = json_decode(file_get_contents('php://input'),true);
 foreach ($body as $key=>$item) {
     $_POST[$key] = $item;
 }
-header("Access-Control-Allow-Headers: Content-Type, Accept, Origin, Referer, User-Agent, Access-Control-Allow-Headers, Authorization, X-Requested-With");
-header("Access-Control-Allow-Methods: *");
+header("Access-Control-Allow-Headers: Content-Type, Accept, Origin, Referer, User-Agent, Access-Control-Allow-Headers, Authorization, X-Requested-With, Range");
+header("Access-Control-Allow-Methods: GET, PUT, POST, HEAD");
 header('Access-Control-Allow-Origin: *');
 require_once __DIR__ . '/../apiHeadSecure.php';
 
