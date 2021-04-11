8f0a11a9faed4f864b94134e1a57e5a01665d11e
James Bithell <james@bithell.com>
2021-04-11 18:53:22 +0100

    Add more allowed headers

--- html/admin/api/s3files/generateSignatureUppy.php
+++ html/admin/api/s3files/generateSignatureUppy.php
@@ -4,7 +4,7 @@ $body = json_decode(file_get_contents('php://input'),true);
 foreach ($body as $key=>$item) {
     $_POST[$key] = $item;
 }
-header("Access-Control-Allow-Headers: Content-Type, Accept, Origin, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+header("Access-Control-Allow-Headers: Content-Type, Accept, Origin, Referer, User-Agent, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Access-Control-Allow-Methods: *");
 header('Access-Control-Allow-Origin: *');
 require_once __DIR__ . '/../apiHeadSecure.php';
