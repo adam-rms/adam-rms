c6282c85708952c24e7ef2635128956be27f3d55
James Bithell <james@bithell.com>
2021-04-11 19:38:40 +0100

    Set the specific origin for the header, as safari *might* not like wildcards

--- html/admin/api/s3files/generateSignatureUppy.php
+++ html/admin/api/s3files/generateSignatureUppy.php
@@ -4,9 +4,11 @@ $body = json_decode(file_get_contents('php://input'),true);
 foreach ($body as $key=>$item) {
     $_POST[$key] = $item;
 }
+$origin = $_SERVER['HTTP_ORIGIN'];
 header("Access-Control-Allow-Headers: Content-Type, Accept, Origin, Referer, User-Agent, Access-Control-Allow-Headers, Authorization, X-Requested-With, Range, Content-Length, Accept-Language, Host, Accept-Encoding, Connection");
 header("Access-Control-Allow-Methods: GET, PUT, POST, HEAD");
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: ' . $origin);
+$headersSet = true;
 require_once __DIR__ . '/../apiHeadSecure.php';
 
 $bucket = $CONFIG['AWS']['DEFAULTUPLOADS']['BUCKET'];
