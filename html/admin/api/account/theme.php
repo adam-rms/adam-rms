a1c5e045d23dd1e6992d41ccb8e0aca83eb1caad
Robert Watts <mail@rlwatts.co.uk>
2021-05-26 19:39:18 +0100

    Remove the theme audit log as it is not needed to business performance

--- html/admin/api/account/theme.php
+++ html/admin/api/account/theme.php
@@ -9,8 +9,6 @@
 
     $DBLIB->where("users_userid", $userid);
     if ($DBLIB->update ('users', ["users_dark_mode" => $isDarkTheme ? 1 : 0])) {
-        $bCMS->auditLog("UPDATE", "users", "CHANGED THEME TO " . $isDarkTheme ? "DARK" : "LIGHT", $AUTH->data['users_userid'],$userid);
-
         die("1");
     }
     else die("0");
