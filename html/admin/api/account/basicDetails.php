9326e18ca0d09aaeb5f38c9779f9f7a580a5ba1a
James Bithell <Jbithell@users.noreply.github.com>
2021-05-28 06:53:08 -0700

    Move from Account Page to a new unified user page (#107)
    * Remove link to account page on nav to make space for dark mode
    
    * Remove ability to create new users
    
    * Complete account endpoint audit and migration to new user page

--- html/admin/api/account/basicDetails.php
+++ html/admin/api/account/basicDetails.php
@@ -1,70 +1,35 @@
 <?php
 require_once __DIR__ . '/../apiHeadSecure.php';
 
-header('Content-Type:text/plain');
+$array = [];
+if (!isset($_POST['formData'])) die("404");
+foreach ($_POST['formData'] as $item) {
+    if ($item['value'] == '') $item['value'] = null;
+    $array[$item['name']] = $item['value'];
+}
 
-if (isset($_POST['username'])) {
-    if ($_POST['userid'] == "NEW" and $AUTH->permissionCheck(4)) $newUser = true; //Are we making a new user here?
-    else $newUser = false;
+if (!$AUTH->permissionCheck(5)) $array['users_userid'] = $AUTH->data['users_userid'];
 
-    if ($AUTH->permissionCheck(5) && $PAGEDATA['USERDATA']['users_userid'] != $_POST['userid'] && !$newUser) {
-        $DBLIB->where("users_userid", $bCMS->sanitizeString($_POST['userid']));
-        $thisUser = $DBLIB->getone("users", ["users_userid", "users_email", "users_username"]);
-        if (!$thisUser) die("5");
-        $userid = $thisUser["users_userid"];
-    } else {
-        $userid = $PAGEDATA['USERDATA']['users_userid'];
-        $thisUser = false;
-    }
+$DBLIB->where("users.users_deleted", 0);
+$DBLIB->where("users.users_suspended", 0);
+$DBLIB->where("users.users_userid", $array['users_userid']);
+$currentUserData = $DBLIB->getone("users", ["users.*"]);
+if (!$currentUserData) finish(false, ["message" => "User not found error"]);
 
+$array["users_email"] = strtolower($array["users_email"]);
+$array["users_username"] = strtolower($array["users_username"]);
 
-    if (
-        (
-            (!$newUser && !$thisUser && strtolower($_POST['email']) != $PAGEDATA['USERDATA']['users_email']) //This users' user account
-          or ($thisUser && strtolower($_POST['email']) != $thisUser['users_email']) //Existing user account being edited
-            or $newUser
-        ) && $AUTH->emailTaken($bCMS->sanitizeString(strtolower($_POST['email'])))) die("Email taken");
-    elseif (
-        (
-            (!$newUser && !$thisUser && strtolower($_POST['username']) != $PAGEDATA['USERDATA']['users_username']) //This users' user account
-            or ($thisUser && strtolower($_POST['username']) != $thisUser['users_username']) //Existing user account being edited
-            or $newUser
-        ) && $AUTH->usernameTaken($bCMS->sanitizeString(strtolower($_POST['username'])))) die("Username taken");
-    else {
-        $data = Array (
-            'users_email' => strtolower($bCMS->sanitizeString($_POST['email'])),
-            'users_username' => strtolower($bCMS->sanitizeString($_POST['username'])),
-            'users_name1' => $bCMS->sanitizeString($_POST['forename']),
-            'users_name2' => $bCMS->sanitizeString($_POST['lastname'])
-        );
+if ($array["users_email"] != $currentUserData["users_email"] and $AUTH->emailTaken($array["users_email"])) finish(false, ["message" => "Sorry this email is in use for another account"]);
+if ($array["users_username"] != $currentUserData["users_username"] and $AUTH->usernameTaken($array["users_username"])) finish(false, ["message" => "Sorry this username is in use for another account, please pick another"]);
 
+if ($array["users_email"] != $currentUserData["users_email"]) $array["users_emailVerified"] = 0;
+else $array["users_emailVerified"] = $currentUserData["users_emailVerified"];
 
-
-        if (!$newUser) {
-            $DBLIB->where('users_userid', $userid);
-            if ($DBLIB->update('users', $data)) {
-                if (($thisUser && $thisUser['users_email'] != strtolower($_POST['email'])) or (!$thisUser && strtolower($_POST['email']) != $PAGEDATA['USERDATA']['users_email'])) {
-                    //The email address has been changed
-                    $DBLIB->where ('users_userid', $userid);
-                    $DBLIB->update ('users', ["users_emailVerified" => "0"]); //Set E-Mail to unverified
-                    $AUTH->verifyEmail($userid);
-                }
-                $bCMS->auditLog("UPDATE", "users", json_encode($data), $AUTH->data['users_userid'],$userid);
-                die('1');
-            } else die("2");
-        } else {
-            $data["users_salty1"] = $bCMS->randomString(8);
-            $data["users_salty2"] = $bCMS->randomString(8);
-            $data["users_hash"] = $CONFIG['nextHash'];
-            $data["users_password"] = "RESET";
-            $newUser = $DBLIB->insert("users", $data);
-            if (!$newUser) die("6");
-            else {
-                $bCMS->auditLog("INSERT", "users", json_encode($data), $AUTH->data['users_userid'],$newUser);
-                die("" . json_encode(["result" => true, "newUserId" => $newUser]));
-            }
-        }
-
-    }
-} else die('3');
-?>
+$DBLIB->where("users_userid",$array['users_userid']);
+$result = $DBLIB->update("users", array_intersect_key( $array, array_flip( ["users_username","users_name1","users_name2","users_email","users_social_facebook","users_social_twitter","users_social_instagram","users_social_linkedin","users_social_snapchat"])),1);
+if (!$result) finish(false, ["code" => "UPDATE-FAIL", "message"=> "Could not update account details"]);
+else {
+    if ($array["users_emailVerified"] == 0) $AUTH->verifyEmail($array['users_userid']);
+    $bCMS->auditLog("EDIT-ACCOUNT", "users", json_encode($array), $AUTH->data['users_userid'],$array['users_userid']);
+    finish(true);
+}
