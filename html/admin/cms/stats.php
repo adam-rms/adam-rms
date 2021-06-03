bf3b95087a541e5657a0a1f48874c4f1eedda39c
John Cherry <github@johncherry.me>
2021-06-03 20:47:01 +0100

    Some Basic stats

--- html/admin/cms/stats.php
+++ html/admin/cms/stats.php
@@ -4,13 +4,30 @@ require_once __DIR__ . '/../common/headSecure.php';
 if (!$AUTH->instancePermissionCheck(125)) die($TWIG->render('404.twig', $PAGEDATA));
 
 if (!isset($_GET['p']) or strlen($_GET['p']) < 1) die($TWIG->render('404.twig', $PAGEDATA));
-$DBLIB->where("instances_id",$AUTH->data['instance']['instances_id']);
-$DBLIB->where("cmsPages_deleted",0);
-$DBLIB->where("cmsPages_archived",0);
-if ($AUTH->data['instance']["instancePositions_id"]) $DBLIB->where("(cmsPages_visibleToGroups IS NULL OR (FIND_IN_SET(" . $AUTH->data['instance']["instancePositions_id"] . ", cmsPages_visibleToGroups) > 0))");
+
+//get all views for page
 $DBLIB->where("cmsPages_id",$_GET['p']);
-$PAGEDATA['PAGE'] = $DBLIB->getOne("cmsPages");
-if (!$PAGEDATA['PAGE']) die($TWIG->render('404.twig', $PAGEDATA));
+$views = $DBLIB->get("cmspagesviews");
+
+//declare stats array
+$PAGEDATA['stats'] = [
+    'today'=>0,
+    'week'=>0,
+];
+
+foreach ($views as $key => $view){
+    $date = new DateTime($view['cmsPagesViews_timestamp']);
+    $date = $date->format('Y-m-d');
+    if ($date == date("Y-m-d")){
+        $PAGEDATA['stats']['today'] ++;
+        $PAGEDATA['stats']['week'] ++;
+    } elseif ($date > date('Y-m-d', strtotime("-7 days"))){
+        $PAGEDATA['stats']['week'] ++;
+    }
+
+}
+
+print_r($PAGEDATA['stats']);
 
 echo $TWIG->render('cms/cms_stats.twig', $PAGEDATA);
 ?>
