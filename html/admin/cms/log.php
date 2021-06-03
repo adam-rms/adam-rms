2e0272b9d1a285162b3784a495f6f718d9ca5e66
John Cherry <github@johncherry.me>
2021-06-03 22:50:08 +0100

    Added Views by User stat

--- html/admin/cms/log.php
+++ html/admin/cms/log.php
@@ -5,13 +5,13 @@ if (!$AUTH->instancePermissionCheck(128)) die($TWIG->render('404.twig', $PAGEDAT
 
 if (!isset($_GET['p']) or strlen($_GET['p']) < 1) die($TWIG->render('404.twig', $PAGEDATA));
 
+$PAGEDATA['cmsPages_id'] = $_GET['p'];
 $DBLIB->where("cmsPages_id",$_GET['p']);
 
 $PAGEDATA['VIEWERS'] = $DBLIB->get("cmspagesviews");
 
 foreach ($PAGEDATA['VIEWERS'] as $key => $view){
     $DBLIB->where("users_userid", $view['users_userid']);
-
     $PAGEDATA['VIEWERS'][$key]['user'] = $DBLIB->getOne("users", "users_name1, users_name2");
 }
 
--- html/admin/cms/stats.php
+++ html/admin/cms/stats.php
@@ -5,29 +5,60 @@ if (!$AUTH->instancePermissionCheck(125)) die($TWIG->render('404.twig', $PAGEDAT
 
 if (!isset($_GET['p']) or strlen($_GET['p']) < 1) die($TWIG->render('404.twig', $PAGEDATA));
 
+$PAGEDATA['cmsPages_id'] = $_GET['p'];
 //get all views for page
 $DBLIB->where("cmsPages_id",$_GET['p']);
 $views = $DBLIB->get("cmspagesviews");
 
+/**
+ * Hello future editor of this
+ * Obviously, fetching this information as SQL queries would be better for pageload times.
+ * This method of using php to do the calculations is easier to expand on, and so is
+ * hopefully easier for you to play with the cms page data.
+ *
+ * Enjoy
+ */
+
+
 //declare stats array
+$accessedUsers = [];
 $PAGEDATA['stats'] = [
-    'today'=>0,
-    'week'=>0,
+    'today' => 0,
+    'week' => 0,
+    'all' => 0,
 ];
 
+//Create actual stats here
 foreach ($views as $key => $view){
     $date = new DateTime($view['cmsPagesViews_timestamp']);
-    $date = $date->format('Y-m-d');
-    if ($date == date("Y-m-d")){
+    $date = $date->format('Y-m-d'); //get the timestamp as just a date for comparisons
+    if ($date == date("Y-m-d")){ //is today
         $PAGEDATA['stats']['today'] ++;
         $PAGEDATA['stats']['week'] ++;
-    } elseif ($date > date('Y-m-d', strtotime("-7 days"))){
+    } elseif ($date > date('Y-m-d', strtotime("-7 days"))){//is past week
         $PAGEDATA['stats']['week'] ++;
     }
+    $PAGEDATA['stats']['all'] ++; //all records
+
+    //This bit is for views by user
+    if (array_key_exists($view['users_userid'], $accessedUsers)){
+        //user already exists so just increment
+        $accessedUsers[$view['users_userid']]['accessed'] ++;
+    } else {
+        //add new user + id combination to accessed user list
+        $accessedUsers[$view['users_userid']]['accessed'] = 1;
+
+        //get username
+        $accessedUsers[$view['users_userid']]['users_userid'] = $view['users_userid'];
+        $DBLIB->where("users_userid", $view['users_userid']);
+        $accessedUsers[$view['users_userid']]['username'] = $DBLIB->getOne("users", "users_name1, users_name2");
+    }
+    //update timestamp as should be newer - should probs check this but oh well
+    $accessedUsers[$view['users_userid']]['last'] = $view['cmsPagesViews_timestamp'];
 
 }
-
-print_r($PAGEDATA['stats']);
+//add user data to page data
+$PAGEDATA['userViews'] = $accessedUsers;
 
 echo $TWIG->render('cms/cms_stats.twig', $PAGEDATA);
 ?>
