83a8e621e7903e3eeb7e9fbb8ce7365e7b260f0b
John Cherry <github@johncherry.me>
2021-06-03 20:24:52 +0100

    Wrong permission id for 404 error

--- html/admin/cms/log.php
+++ html/admin/cms/log.php
@@ -1,7 +1,7 @@
 <?php
 require_once __DIR__ . '/../common/headSecure.php';
 
-if (!$AUTH->instancePermissionCheck(125)) die($TWIG->render('404.twig', $PAGEDATA));
+if (!$AUTH->instancePermissionCheck(128)) die($TWIG->render('404.twig', $PAGEDATA));
 
 if (!isset($_GET['p']) or strlen($_GET['p']) < 1) die($TWIG->render('404.twig', $PAGEDATA));
 
@@ -14,6 +14,6 @@ foreach ($PAGEDATA['VIEWERS'] as $key => $view){
 
     $PAGEDATA['VIEWERS'][$key]['user'] = $DBLIB->getOne("users", "users_name1, users_name2");
 }
-//print_r($PAGEDATA['VIEWERS']);
+
 echo $TWIG->render('cms/cms_log.twig', $PAGEDATA);
 ?>
