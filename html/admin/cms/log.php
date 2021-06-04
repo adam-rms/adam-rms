0477ec36184d24d00727f92d0068ce2a5172f578
James Bithell <james@bithell.com>
2021-06-04 08:49:21 +0100

    Prevent users viewing stats for pages not in their instance

--- html/admin/cms/log.php
+++ html/admin/cms/log.php
@@ -5,7 +5,15 @@ if (!$AUTH->instancePermissionCheck(128)) die($TWIG->render('404.twig', $PAGEDAT
 
 if (!isset($_GET['p']) or strlen($_GET['p']) < 1) die($TWIG->render('404.twig', $PAGEDATA));
 
-$PAGEDATA['cmsPages_id'] = $_GET['p'];
+$DBLIB->where("instances_id",$AUTH->data['instance']['instances_id']);
+$DBLIB->where("cmsPages_deleted",0);
+$DBLIB->where("cmsPages_archived",0);
+if ($AUTH->data['instance']["instancePositions_id"]) $DBLIB->where("(cmsPages_visibleToGroups IS NULL OR (FIND_IN_SET(" . $AUTH->data['instance']["instancePositions_id"] . ", cmsPages_visibleToGroups) > 0))");
+$DBLIB->where("cmsPages_id",$_GET['p']);
+$PAGEDATA['PAGE'] = $DBLIB->getOne("cmsPages",["cmsPages_id"]);
+if (!$PAGEDATA['PAGE']) die($TWIG->render('404.twig', $PAGEDATA));
+
+
 $DBLIB->orderBy("cmsPagesViews_timestamp","ASC");
 $DBLIB->join("users","cmsPagesViews.users_userid=users.users_userid","LEFT");
 $DBLIB->where("cmsPages_id",$_GET['p']);
