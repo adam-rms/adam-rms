9dbe19dfa5b6f05c86bb8130eae4e055dcb22129
John Cherry <github@johncherry.me>
2021-06-07 17:23:13 +0100

    Refactor previous commit (48fb88) to archive more efficiently

--- html/admin/api/projects/unArchive.php
+++ html/admin/api/projects/unArchive.php
@@ -3,15 +3,10 @@ require_once __DIR__ . '/../apiHeadSecure.php';
 
 if (!$AUTH->instancePermissionCheck(25) or !isset($_POST['projects_id'])) die("404");
 
-//unArchive any sub-projects at the same time
-$DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
-$DBLIB->where("projects.projects_deleted", 0);
-$DBLIB->where("projects.projects_parent_project_id", $_POST['projects_id']);
-$subprojects = $DBLIB->update("projects", ["projects.projects_archived" => 0]);
-
 $DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
 $DBLIB->where("projects.projects_deleted", 0);
 $DBLIB->where("projects.projects_id", $_POST['projects_id']);
+$DBLIB->orWhere("projects.projects_parent_project_id", $_POST['projects_id']);
 $project = $DBLIB->update("projects", ["projects.projects_archived" => 0]);
 if (!$project) finish(false);
 
