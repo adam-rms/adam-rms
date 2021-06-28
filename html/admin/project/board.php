566b55b8363286fcc7155c79d7236095532beba8
John Cherry <github@johncherry.me>
2021-06-28 12:33:14 +0100

    Asset statuses can be updated in the board view

--- html/admin/project/board.php
+++ html/admin/project/board.php
@@ -5,6 +5,8 @@ if (!$AUTH->instancePermissionCheck(20) or !isset($_GET['id'])) die($TWIG->rende
 
 require_once __DIR__ . '/../api/projects/data.php'; //Where most of the data comes from
 
+if (count($PAGEDATA['FINANCIALS']['assetsAssigned']) == 0) die($TWIG->render('404.twig', $PAGEDATA));
+
 foreach ($PAGEDATA['assetsAssignmentsStatus'] as $status) {
     $assets=[];
     foreach ($PAGEDATA['FINANCIALS']['assetsAssigned'][1]['assets'] as $asset){
