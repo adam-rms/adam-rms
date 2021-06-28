2a410d4dc47ff5ef404a5126ed668028c0baab6b
John Cherry <github@johncherry.me>
2021-06-28 16:05:14 +0100

    Allow multiple asset types to actually be listed

--- html/admin/project/board.php
+++ html/admin/project/board.php
@@ -1,17 +1,19 @@
 <?php
 ini_set('memory_limit','2048M');
 require_once __DIR__ . '/../common/headSecure.php';
-if (!$AUTH->instancePermissionCheck(20) or !isset($_GET['id'])) die($TWIG->render('404.twig', $PAGEDATA));
+if (!$AUTH->instancePermissionCheck(20) || !isset($_GET['id'])) {die($TWIG->render('404.twig', $PAGEDATA));}
 
 require_once __DIR__ . '/../api/projects/data.php'; //Where most of the data comes from
 
-if (count($PAGEDATA['FINANCIALS']['assetsAssigned']) == 0) die($TWIG->render('404.twig', $PAGEDATA));
+if (count($PAGEDATA['FINANCIALS']['assetsAssigned']) == 0) {die($TWIG->render('404.twig', $PAGEDATA));}
 
 foreach ($PAGEDATA['assetsAssignmentsStatus'] as $status) {
     $assets=[];
-    foreach ($PAGEDATA['FINANCIALS']['assetsAssigned'][1]['assets'] as $asset){
-        if ($asset['assetsAssignmentsStatus_name'] == $status['assetsAssignmentsStatus_name']){
-            array_push($assets, $asset);
+    foreach ($PAGEDATA['FINANCIALS']['assetsAssigned'] as $asset){
+        foreach ($asset['assets'] as $assetSUB){
+            if ($assetSUB['assetsAssignmentsStatus_name'] == $status['assetsAssignmentsStatus_name']){
+                array_push($assets, $assetSUB);
+            }
         }
     }
     $PAGEDATA['assetsAssignmentsStatus'][$status['assetsAssignmentsStatus_id'] - 1]['assets'] = $assets;
