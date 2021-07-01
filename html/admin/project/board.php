dc65f840089c1c5e030c0dd57cf4c7ff0221f315
John Cherry <github@johncherry.me>
2021-07-01 18:10:02 +0100

    Asset boards support sub hires

--- html/admin/project/board.php
+++ html/admin/project/board.php
@@ -16,7 +16,26 @@ foreach ($PAGEDATA['assetsAssignmentsStatus'] as $status) {
             }
         }
     }
-    $PAGEDATA['assetsAssignmentsStatus'][$status['assetsAssignmentsStatus_id'] - 1]['assets'] = $assets;
+    $PAGEDATA['assetsAssignmentsStatus'][$status['assetsAssignmentsStatus_order'] - 1]['assets'] = $assets;
+}
+
+foreach ($PAGEDATA['FINANCIALS']['assetsAssignedSUB'] as $instance){
+    $DBLIB->orderBy("assetsAssignmentsStatus_order","ASC");
+    $DBLIB->where("assetsAssignmentsStatus.instances_id", $instance['instance']['instances_id']);
+    $PAGEDATA['FINANCIALS']['assetsAssignedSUB'][$instance['instance']['instances_id']]['statuses'] = $DBLIB->get("assetsAssignmentsStatus");
+    foreach ($PAGEDATA['FINANCIALS']['assetsAssignedSUB'][$instance['instance']['instances_id']]['statuses'] as $status){
+        $assets=[];
+        foreach ($instance['assets'][$instance['instance']['instances_id'] + 1]['assets'] as $asset){
+            //if asset status is null, add to the first column
+            if ($asset['assetsAssignmentsStatus_name'] == null && $status['assetsAssignmentsStatus_order'] == 1){
+                array_push($assets, $asset);
+            }
+            if ($asset['assetsAssignmentsStatus_name'] == $status['assetsAssignmentsStatus_name']){
+                array_push($assets, $asset);
+            }
+        }
+        $PAGEDATA['FINANCIALS']['assetsAssignedSUB'][$instance['instance']['instances_id']]['statuses'][$status['assetsAssignmentsStatus_order'] - 1]['assets'] = $assets;
+    }
 }
 
 echo $TWIG->render('project/project_asset_board.twig', $PAGEDATA);
