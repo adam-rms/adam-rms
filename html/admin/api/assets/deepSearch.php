d5048aaaa352a905838083742f039e0d8b5f4986
James Bithell <james@bithell.com>
2021-07-21 19:34:19 +0100

    Closes https://github.com/bstudios/adam-rms/issues/150

--- html/admin/api/assets/deepSearch.php
+++ html/admin/api/assets/deepSearch.php
@@ -1,7 +1,8 @@
 <?php
 require_once __DIR__ . '/../apiHead.php';
 
-if ($AUTH->login and isset($_POST['projectid']) and is_int($_POST['projectid']) and $_POST['projectid'] != $AUTH->data["users_selectedProjectID"]) {
+
+if ($AUTH->login and isset($_POST['projectid']) and is_numeric($_POST['projectid']) and $_POST['projectid'] != $AUTH->data["users_selectedProjectID"]) {
     $DBLIB->where("users_userid", $AUTH->data['users_userid']);
     $update = $DBLIB->update("users", ["users_selectedProjectID" => $_POST['projectid']], 1);
     if (!$update) finish(false,["message"=>"Selected project change error"]);
