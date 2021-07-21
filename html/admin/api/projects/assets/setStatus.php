5707015acf04d446264a17f7b0846abe88b8d560
James Bithell <james@bithell.com>
2021-07-21 17:48:08 +0100

    Correct permissions and move to ternary

--- html/admin/api/projects/assets/setStatus.php
+++ html/admin/api/projects/assets/setStatus.php
@@ -10,12 +10,12 @@ if ($_POST['status_is_order']){
     $DBLIB->where("assetsAssignmentsStatus_deleted", 0);
     $DBLIB->where("instances_id", $AUTH->data['instance']['instances_id']);
     $status_id = $DBLIB->getOne("assetsAssignmentsStatus", "assetsAssignmentsStatus_id")['assetsAssignmentsStatus_id'];
-}
+} else $status_id = false;
 
 $DBLIB->where("assetsAssignments_id", $_POST['assetsAssignments_id']);
 $DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
 $DBLIB->where("projects.projects_deleted", 0);
 $DBLIB->join("projects", "assetsAssignments.projects_id=projects.projects_id", "LEFT");
-$assignment = $DBLIB->update("assetsAssignments", ["assetsAssignmentsStatus_id" => (($_POST['status_is_order']) ? $status_id :  $_POST['assetsAssignments_status']) ]);
+$assignment = $DBLIB->update("assetsAssignments", ["assetsAssignmentsStatus_id" => ($status_id ?? $_POST['assetsAssignments_status']) ]);
 if (!$assignment) finish(false);
 else finish(true);
