6dc3b9bd7ad27ae60f342d73c6f91a12ee8c16ca
John Cherry <github@johncherry.me>
2021-07-21 19:14:35 +0100

    Added status listing (#128)
    * Added status listing
    
    * change how unassigned assets are tracked
    
    * Create scrollable cards
    
    * Populate cards from asset statuses
    
    * Asset statuses can be updated in the board view
    
    * Allow multiple asset types to actually be listed
    
    * Use jQuery UI to allow drag and drop sorting
    
    * Remove hard-coded statuses which are a bizarre relic
    
    * asset statuses can be updated by asset
    
    * Asset boards support sub hires
    
    * Asset statuses can be edited on a per-instance basis
    
    NB. This may cause a small issue with any status sorting due to a 0-index error in older code
    
    * Allow asset statuses to be renamed + bugfixes
    
    * Correct case of assetsassignmentsstatus
    
    * Move edit functions to own folder and clarify permissions
    
    * Fix missing permissions on board
    
    * Remove line break removal merge issue
    
    * Correct permissions and move to ternary
    
    * Move board into main project page

---
+++ html/admin/api/instances/assetAssignmentStatus/edit.php
@@ -0,0 +1,12 @@
+<?php
+require_once __DIR__ . '/../../apiHeadSecure.php';
+
+if (!$AUTH->instancePermissionCheck(83) or !isset($_POST['statusName']) or !isset($_POST['statusId'])) finish(false);
+
+$DBLIB->where("instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->where("assetsAssignmentsStatus_deleted", 0);
+$DBLIB->where("assetsAssignmentsStatus_id", $_POST['statusId']);
+$updateQuery = $DBLIB->update("assetsAssignmentsStatus", ["assetsAssignmentsStatus_name" => $_POST['statusName']]);
+
+if (!$updateQuery) finish(false, ["code" => "UPDATE-STATUS-FAIL", "message"=> "Could not Update asset status"]);
+finish(true);
