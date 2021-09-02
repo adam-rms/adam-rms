f048bd95dd98a0217130ee019f890a2955778301
James Bithell <james@bithell.com>
2021-09-02 14:47:19 +0100

    Only update assignments when they're not deleted anyway, that seems sensible

--- html/admin/api/projects/assets/setStatus.php
+++ html/admin/api/projects/assets/setStatus.php
@@ -16,11 +16,11 @@ if (isset($_POST['assetsAssignments_id'])){
     $DBLIB->where("assetsAssignments_id", $_POST['assetsAssignments_id'], (is_array($_POST['assetsAssignments_id'])? 'IN' : '='));
 } elseif (isset($_POST['projects_id'])) {
     $DBLIB->where("assetsAssignments.projects_id", $_POST['projects_id']);
-    $DBLIB->where("assetsAssignments.assetsAssignments_deleted", 0);
 } else {
     finish(false);
 }
 
+$DBLIB->where("assetsAssignments.assetsAssignments_deleted", 0);
 $DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
 $DBLIB->where("projects.projects_deleted", 0);
 $DBLIB->join("projects", "assetsAssignments.projects_id=projects.projects_id", "LEFT");
