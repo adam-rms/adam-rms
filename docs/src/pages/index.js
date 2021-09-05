4a17b70bb6f1a090be8936a0c242637bc1621bc4
James Bithell <james@bithell.com>
2021-09-05 18:06:59 +0100

    Update index.js

--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -38,13 +38,13 @@ function Pricing() {
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
         <h1 className="hero__title">Pricing</h1>
-        <p className="hero__subtitle">AdamRMS is currently offered as a hosted solution, by Bithell Studios Ltd, and as a self-hosted solution using the docker images published on Github.<br/>We are keen to talk to new customers about hosting AdamRMS for you - get in touch to discuss your use case.</p>
+        <p className="hero__subtitle">AdamRMS is currently offered as a cost-price hosted solution, by Bithell Studios Ltd, and as a free self-hosted solution using the code published on Github. Please note that the hosted solution is not currently taking on new customers.</p>
         <div className={styles.buttons}>
-          <a
+          <Link
             className="button button--secondary button--lg"
-            href="mailto:studios@jbithell.com">
-            Talk to sales
-          </a>
+            href="https://github.com/">
+            GitHub Repo
+          </Link>
         </div>
       </div>
     </header>
