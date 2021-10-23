ca898da4a33b59005c85e6857ec5718d6b5953ff
James Bithell <Jbithell@users.noreply.github.com>
2021-10-23 11:31:15 +0100

    Heroku builds

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,9 @@
 import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app.module";
-
+const PORT = process.env.PORT || 8080;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(8080);
+  await app.listen(PORT);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
