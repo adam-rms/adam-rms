1551f27cd670c7183f691654d6804a8fefe6bb1e
Christopher Nethercott <ccnethercott@gmail.com>
2021-11-23 14:00:57 +0000

    fix(api/root-test): fix root test

--- api/src/app.controller.spec.ts
+++ api/src/app.controller.spec.ts
@@ -15,8 +15,8 @@ describe("AppController", () => {
   });
 
   describe("root", () => {
-    it('should return "Hello World!"', () => {
-      expect(appController.getHello()).toBe("Hello World!");
+    it('should return "Pong"', () => {
+      expect(appController.getHello()).toBe("Pong");
     });
   });
 });
