2e6a773d10d702d3561d47aaa38da992673d869a
Robert Watts <mail@rlwatts.co.uk>
2021-12-18 15:18:19 +0000

    Add react helmet and Page Title

---
+++ app/src/PageTitle.tsx
@@ -0,0 +1,34 @@
+import { Helmet } from "react-helmet";
+
+type Props = {
+	title?: string;
+	favicon?: string;
+};
+
+/**
+ *
+ * @param title The page title
+ * @param favicon A custom Favicon for the page you're on
+ */
+const PageTitle = ({ title, favicon }: Props) => {
+	return (
+		<Helmet>
+			<title>
+				{title && `${title} - `}AdamRMS
+			</title>
+			{favicon ? (
+				<link rel="shortcut icon" href={favicon} />
+			) : (
+				<link
+					rel="shortcut icon"
+					type="image/png"
+					href="/assets/icon/favicon.png"
+					sizes="32x32"
+				/>
+			)}
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+		</Helmet>
+	);
+};
+
+export default PageTitle;
