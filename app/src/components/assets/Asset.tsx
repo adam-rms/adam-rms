8bde02aad93ed54b1ada26b8c79768e42519520a
John Cherry <jac677@york.ac.uk>
2021-12-18 17:45:29 +0000

    Add message when no assets found (#261)
    * Add message when no assets found
    
    * Allow assets to be accessed directly
    
    * Fix merge conflict
    
    * Remove Import
    
    Co-authored-by: James Bithell <Jbithell@users.noreply.github.com>
    Co-authored-by: Robert Watts <11817698+Robert-Watts@users.noreply.github.com>
    Co-authored-by: Robert Watts <mail@rlwatts.co.uk>

--- app/src/components/assets/Asset.tsx
+++ app/src/components/assets/Asset.tsx
@@ -23,131 +23,146 @@ const Asset = () => {
     //filter by requested asset type
     const thisAssetType = AssetTypes.assets.find((element: IAssetTypeData) => element.assetTypes_id == parseInt(type));
 
-    //filter by requested asset
-    const thisAsset = thisAssetType.tags.find((element: IAsset) => element.assets_id == parseInt(asset));
+    //if we've got the asset, show data
+    if (thisAssetType) {
 
-    //return page layout
-    return (
-        <Page title={thisAsset.assets_tag_format}>
-            <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
-                <IonRefresherContent/>
-            </IonRefresher>
+        //filter by requested asset
+        const thisAsset = thisAssetType.tags.find((element: IAsset) => element.assets_id == parseInt(asset));
 
-            {/* Maintenance */}
-            {thisAsset.flagsblocks.BLOCK.map((block: any) => {
-                return (
-                    <IonCard key={block.maintenanceJobs_id}>
-                        <IonCardContent>
-                            <IonCardTitle><FontAwesomeIcon icon={faBan} color="#dc3545" /> {block.maintenanceJobs_title}</IonCardTitle>
-                            <IonLabel className="container">{block.maintenanceJobs_faultDescription}</IonLabel>
-                        </IonCardContent>
-                    </IonCard>
-                )
-            })}
-            {thisAsset.flagsblocks.FLAG.map((block: any) => {
-                return (
-                    <IonCard key={block.maintenanceJobs_id}>
-                        <IonCardContent>
-                            <IonCardTitle><FontAwesomeIcon icon={faFlag} color="#ffc107" /> {block.maintenanceJobs_title}</IonCardTitle>
-                            <IonLabel className="container">{block.maintenanceJobs_faultDescription}</IonLabel>
-                        </IonCardContent>
-                    </IonCard>
-                )
-            })}
+        //return page layout
+        return (
+            <Page title={thisAsset.assets_tag_format}>
+                <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
+                    <IonRefresherContent/>
+                </IonRefresher>
 
-            {/* Asset Notes */}
-            <IonCard>
-                <IonCardContent>
-                    <IonCardTitle>
-                        {thisAsset.assets_notes}
-                    </IonCardTitle>
-                </IonCardContent>
-            </IonCard>
+                {/* Maintenance */}
+                {thisAsset.flagsblocks.BLOCK.map((block: any) => {
+                    return (
+                        <IonCard key={block.maintenanceJobs_id}>
+                            <IonCardContent>
+                                <IonCardTitle><FontAwesomeIcon icon={faBan} color="#dc3545" /> {block.maintenanceJobs_title}</IonCardTitle>
+                                <IonLabel className="container">{block.maintenanceJobs_faultDescription}</IonLabel>
+                            </IonCardContent>
+                        </IonCard>
+                    )
+                })}
+                {thisAsset.flagsblocks.FLAG.map((block: any) => {
+                    return (
+                        <IonCard key={block.maintenanceJobs_id}>
+                            <IonCardContent>
+                                <IonCardTitle><FontAwesomeIcon icon={faFlag} color="#ffc107" /> {block.maintenanceJobs_title}</IonCardTitle>
+                                <IonLabel className="container">{block.maintenanceJobs_faultDescription}</IonLabel>
+                            </IonCardContent>
+                        </IonCard>
+                    )
+                })}
 
-            {/* Asset Data */}
-            <IonCard>
-                <IonCardHeader>
-                    <IonCardTitle>Asset Information</IonCardTitle>
-                </IonCardHeader>
-                <IonCardContent>
-                    <IonList>
-                        <IonRow>
-                            <IonCol>
-                                <IonItem>
-                                    <div className="container">
-                                        <IonCardSubtitle>Mass</IonCardSubtitle>
-                                        <IonCardTitle>{thisAsset.assets_mass_format}</IonCardTitle>
-                                    </div>
-                                </IonItem>
-                                <IonItem>
-                                    <div className="container">
-                                        <IonCardSubtitle>value</IonCardSubtitle>
-                                        <IonCardTitle>{thisAsset.assets_value_format}</IonCardTitle>
-                                    </div>
-                                </IonItem>
-                                <IonItem>
-                                    <div className="container">
-                                        <IonCardSubtitle>Day Rate</IonCardSubtitle>
-                                        <IonCardTitle>{thisAsset.assets_dayRate_format}</IonCardTitle>
-                                    </div>
-                                </IonItem>
-                                <IonItem>
-                                    <div className="container">
-                                        <IonCardSubtitle>Week Rate</IonCardSubtitle>
-                                        <IonCardTitle>{thisAsset.assets_weekRate_format}</IonCardTitle>
-                                    </div>
-                                </IonItem>
-                            </IonCol>
-                            <IonCol>
-                                {thisAssetType.fields.map((element: {}, index: number) => {
-                                    if (thisAssetType.fields[index-1] !== "" && thisAsset["asset_definableFields_" + index] !== "") {
-                                        return (
-                                            <IonItem key={index}>
-                                                <div className="container">
-                                                    <IonCardSubtitle>{thisAssetType.fields[index-1]}</IonCardSubtitle>
-                                                    <IonCardTitle>{thisAsset["asset_definableFields_" + index]}</IonCardTitle>
-                                                </div>
-                                            </IonItem>
-                                        );
-                                    }
-                                })}
-                            </IonCol>
-                        </IonRow>
-                    </IonList>
-                </IonCardContent>
-            </IonCard>
+                {/* Asset Notes */}
+                <IonCard>
+                    <IonCardContent>
+                        <IonCardTitle>
+                            {thisAsset.assets_notes}
+                        </IonCardTitle>
+                    </IonCardContent>
+                </IonCard>
 
-            {/* Asset Files */}
-            { thisAsset.files && thisAsset.files.length > 0 &&
+                {/* Asset Data */}
                 <IonCard>
                     <IonCardHeader>
-                        <IonCardTitle>Asset Files</IonCardTitle>
+                        <IonCardTitle>Asset Information</IonCardTitle>
                     </IonCardHeader>
                     <IonCardContent>
                         <IonList>
-                            {thisAsset.files.map(async (item :any) => {
-                                return (
-                                    <a href={ await s3url(item.s3files_id, item.s3files_meta_size)}>
-                                        <IonItem key={item.s3files_id}>
-                                            <IonLabel slot="start">
-                                                <FontAwesomeIcon icon={fileExtensionToIcon(item.s3files_extension)} />
-                                            </IonLabel>
-                                            <IonLabel>
-                                                <h2>{item.s3files_name}</h2>
-                                            </IonLabel>
-                                            <IonLabel slot="end">
-                                                {formatSize(item.s3files_meta_size)}
-                                            </IonLabel>
-                                        </IonItem>
-                                    </a>
-                                )
-                            })}
+                            <IonRow>
+                                <IonCol>
+                                    <IonItem>
+                                        <div className="container">
+                                            <IonCardSubtitle>Mass</IonCardSubtitle>
+                                            <IonCardTitle>{thisAsset.assets_mass_format}</IonCardTitle>
+                                        </div>
+                                    </IonItem>
+                                    <IonItem>
+                                        <div className="container">
+                                            <IonCardSubtitle>value</IonCardSubtitle>
+                                            <IonCardTitle>{thisAsset.assets_value_format}</IonCardTitle>
+                                        </div>
+                                    </IonItem>
+                                    <IonItem>
+                                        <div className="container">
+                                            <IonCardSubtitle>Day Rate</IonCardSubtitle>
+                                            <IonCardTitle>{thisAsset.assets_dayRate_format}</IonCardTitle>
+                                        </div>
+                                    </IonItem>
+                                    <IonItem>
+                                        <div className="container">
+                                            <IonCardSubtitle>Week Rate</IonCardSubtitle>
+                                            <IonCardTitle>{thisAsset.assets_weekRate_format}</IonCardTitle>
+                                        </div>
+                                    </IonItem>
+                                </IonCol>
+                                <IonCol>
+                                    {thisAssetType.fields.map((element: {}, index: number) => {
+                                        if (thisAssetType.fields[index-1] !== "" && thisAsset["asset_definableFields_" + index] !== "") {
+                                            return (
+                                                <IonItem key={index}>
+                                                    <div className="container">
+                                                        <IonCardSubtitle>{thisAssetType.fields[index-1]}</IonCardSubtitle>
+                                                        <IonCardTitle>{thisAsset["asset_definableFields_" + index]}</IonCardTitle>
+                                                    </div>
+                                                </IonItem>
+                                            );
+                                        }
+                                    })}
+                                </IonCol>
+                            </IonRow>
                         </IonList>
                     </IonCardContent>
                 </IonCard>
-            }
-        </Page>
-    );
+
+                    {/* Asset Files */}
+                    { thisAsset.files && thisAsset.files.length > 0 && 
+                        <IonCard>
+                            <IonCardHeader>
+                                <IonCardTitle>Asset Files</IonCardTitle>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                <IonList>
+                                    {thisAsset.files.map(async (item :any) => {
+                                        return (
+                                            <a href={ await s3url(item.s3files_id, item.s3files_meta_size)}>
+                                                <IonItem key={item.s3files_id}>
+                                                    <IonLabel slot="start">
+                                                        <FontAwesomeIcon icon={fileExtensionToIcon(item.s3files_extension)} />
+                                                    </IonLabel>
+                                                    <IonLabel>
+                                                        <h2>{item.s3files_name}</h2>
+                                                    </IonLabel>
+                                                    <IonLabel slot="end">
+                                                        {formatSize(item.s3files_meta_size)}
+                                                    </IonLabel>
+                                                </IonItem>
+                                            </a>
+                                        )
+                                    })}
+                                </IonList>
+                            </IonCardContent>
+                        </IonCard>
+                    }
+                </Page>
+            );
+    } else {
+        //If there isn't an asset, refresh context to see if that helps
+        refreshAssetTypes();
+        //If there is still no assets, it's probably a network issue
+        //TODO: Handle network issues more gracefully
+        
+        return (
+            <Page title="No Asset Found">
+                <IonCardTitle>No Asset Found, Please Wait</IonCardTitle>
+            </Page>
+        )
+    }
 }
 
 
