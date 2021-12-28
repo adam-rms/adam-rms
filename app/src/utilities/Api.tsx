1dfd973439126ced1ab81639741a6fd48caab066
Robert Watts <mail@rlwatts.co.uk>
2021-12-28 18:47:00 +0000

    Add method to API

--- app/src/utilities/Api.tsx
+++ app/src/utilities/Api.tsx
@@ -1,4 +1,4 @@
-import axios from "axios";
+import axios, { Method as MethodType } from "axios";
 
 /* Where is the API hosted?
  * N.B This is a temporary variable to be replaced when auth is added to the app!
@@ -11,14 +11,20 @@ export const baseURL = "http://admin.test/";
  * Get data from AdamRMS API
  * @param endpoint API endpoint
  * @param data any parameter data for the endpoint
+ * @param method the HTTP Method used
  * @returns response data as an Object
  * @link https://adam-rms.com/docs/api/intro for V1 endpoints
  */
-const Api = async (endpoint: string, data: any = {}) => {
-  return axios
-    .get(baseURL + "api/" + endpoint, {
-      params: data,
-    })
+const Api = async (
+  endpoint: string,
+  data: any = {},
+  method: MethodType = "GET",
+) => {
+  return axios({
+    method: method,
+    url: baseURL + "api/" + endpoint,
+    params: data,
+  })
     .then(function (response) {
       if (response.data["result"] == true) {
         return response.data["response"];
