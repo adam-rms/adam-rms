f5266f3191dcf2b3aac77bda0b66177c47fa4483
John Cherry <github@johncherry.me>
2021-12-29 11:07:39 +0000

    Add check for asset list

--- app/src/pages/assets/AssetTypeList.tsx
+++ app/src/pages/assets/AssetTypeList.tsx
@@ -3,6 +3,7 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   IonAvatar,
   IonCard,
+  IonCardTitle,
   IonImg,
   IonInfiniteScroll,
   IonInfiniteScrollContent,
@@ -42,52 +43,68 @@ const AssetTypeList = () => {
     refreshAssetTypes();
   }, []);
 
-  return (
-    <Page title="Asset List">
-      <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
-        <IonRefresherContent />
-      </IonRefresher>
-      <IonCard>
-        <IonList>
-          {AssetTypes.assets.map((item: IAssetTypeData) => {
-            return (
-              <IonItem
-                key={item.assetTypes_id}
-                routerLink={"/assets/" + item.assetTypes_id}
-              >
-                <ThumbnailContainer>
-                  {item.thumbnails.length > 0 && (
-                    <IonAvatar slot="start">
-                      <IonImg
-                        src={item.thumbnails[0].url}
-                        alt={item.assetTypes_name}
-                      />
-                    </IonAvatar>
-                  )}
-                  {item.thumbnails.length == 0 && (
-                    <FontAwesomeIcon icon={faQuestionCircle} size="2x" />
-                  )}
-                </ThumbnailContainer>
-                <IonLabel>
-                  <h2>{item.assetTypes_name}</h2>
-                  <p>{item.assetCategories_name}</p>
-                </IonLabel>
-                <IonLabel slot="end">
-                  <p>x{item.tags.length}</p>
-                </IonLabel>
-              </IonItem>
-            );
-          })}
-        </IonList>
-        <IonInfiniteScroll onIonInfinite={loadData} threshold="100px">
-          <IonInfiniteScrollContent
-            loadingSpinner="bubbles"
-            loadingText="Loading more assets..."
-          />
-        </IonInfiniteScroll>
-      </IonCard>
-    </Page>
-  );
+  if (AssetTypes) {
+    return (
+      <Page title="Asset List">
+        <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
+          <IonRefresherContent />
+        </IonRefresher>
+        <IonCard>
+          <IonList>
+            {AssetTypes.assets.map((item: IAssetTypeData) => {
+              return (
+                <IonItem
+                  key={item.assetTypes_id}
+                  routerLink={"/assets/" + item.assetTypes_id}
+                >
+                  <ThumbnailContainer>
+                    {item.thumbnails.length > 0 && (
+                      <IonAvatar slot="start">
+                        <IonImg
+                          src={item.thumbnails[0].url}
+                          alt={item.assetTypes_name}
+                        />
+                      </IonAvatar>
+                    )}
+                    {item.thumbnails.length == 0 && (
+                      <FontAwesomeIcon icon={faQuestionCircle} size="2x" />
+                    )}
+                  </ThumbnailContainer>
+                  <IonLabel>
+                    <h2>{item.assetTypes_name}</h2>
+                    <p>{item.assetCategories_name}</p>
+                  </IonLabel>
+                  <IonLabel slot="end">
+                    <p>x{item.tags.length}</p>
+                  </IonLabel>
+                </IonItem>
+              );
+            })}
+          </IonList>
+          <IonInfiniteScroll onIonInfinite={loadData} threshold="100px">
+            <IonInfiniteScrollContent
+              loadingSpinner="bubbles"
+              loadingText="Loading more assets..."
+            />
+          </IonInfiniteScroll>
+        </IonCard>
+      </Page>
+    );
+  } else {
+    //If there isn't an asset, refresh context to see if that helps
+    refreshAssetTypes();
+    //If there is still no assets, it's probably a network issue
+    return (
+      <Page title="Asset List">
+        <IonRefresher slot="fixed" onIonRefresh={doRefresh}>
+          <IonRefresherContent />
+        </IonRefresher>
+        <IonCard>
+          <IonCardTitle>No Assets found</IonCardTitle>
+        </IonCard>
+      </Page>
+    );
+  }
 };
 
 const ThumbnailContainer = styled.div`
