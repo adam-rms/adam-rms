55f09d9d9b50b067c368f9aae20e7a58fdb0d43a
James Bithell <Jbithell@users.noreply.github.com>
2022-01-04 19:00:07 +0000

    Block project comment download if project is not instance

--- html/admin/api/projects/getComments.php
+++ html/admin/api/projects/getComments.php
@@ -12,8 +12,14 @@ require_once __DIR__ . '/../apiHeadSecure.php';
 
 if (!$AUTH->instancePermissionCheck(20) or !isset($_POST['projects_id'])) finish(false);
 
+$DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->where("projects.projects_deleted", 0);
+$DBLIB->where("projects.projects_id", $_POST['projects_id']);
+$project = $DBLIB->getone("projects",["projects_id"]);
+if (!$project) finish(false);
+
 $DBLIB->where("auditLog.auditLog_deleted", 0);
-$DBLIB->where("auditLog.projects_id", $_POST['projects_id']);
+$DBLIB->where("auditLog.projects_id", $project['projects_id']);
 $DBLIB->where("auditLog.auditLog_actionType", "QUICKCOMMENT");
 $DBLIB->join("users", "auditLog.users_userid=users.users_userid", "LEFT");
 $DBLIB->orderBy("auditLog.auditLog_timestamp", "DESC");
