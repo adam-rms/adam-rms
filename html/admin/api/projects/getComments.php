bb38e2e62dbd75c566a97dd9edba8eac0dbde01f
John Cherry <jac677@york.ac.uk>
2022-01-04 18:42:58 +0000

    Add audit log endpoint (#288)
    * Add audit log endpoint
    
    * Actually add content
    
    * Change to just comments
    
    * Rename file
    
    Co-authored-by: James Bithell <Jbithell@users.noreply.github.com>

---
+++ html/admin/api/projects/getComments.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * API
+ * \projects\getComments.php
+ * get comment data for given project
+ *
+ * Arguments:
+ *  - projects_id: a project
+ */
+
+require_once __DIR__ . '/../apiHeadSecure.php';
+
+if (!$AUTH->instancePermissionCheck(20) or !isset($_POST['projects_id'])) finish(false);
+
+$DBLIB->where("auditLog.auditLog_deleted", 0);
+$DBLIB->where("auditLog.projects_id", $_POST['projects_id']);
+$DBLIB->where("auditLog.auditLog_actionType", "QUICKCOMMENT");
+$DBLIB->join("users", "auditLog.users_userid=users.users_userid", "LEFT");
+$DBLIB->orderBy("auditLog.auditLog_timestamp", "DESC");
+$DBLIB->orderBy("auditLog.auditLog_id", "DESC");
+$auditLog = $DBLIB->get("auditLog",null, ["auditLog.*", "users.users_name1", "users.users_name2", "users.users_email"]);
+
+finish(true, null, $auditLog);
