96e48f0527fda9cc727ca5049fa7ef446b9a277f
Robert Watts <mail@rlwatts.co.uk>
2022-01-15 16:17:35 +0000

    Search CMS

---
+++ html/common/libs/Search/Search.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once __DIR__ . '/BaseSearch.php';
+require_once __DIR__ . '/SearchCMS.php';
+
+class Search extends BaseSearch
+{
+
+    protected array $providers;
+
+    public function __construct(?int $instance_id = null)
+    {
+        parent::__construct($instance_id);
+
+        $this->providers=[
+            new SearchCMS($instance_id)
+        ];
+    }
+
+    /**
+     * Run load on each of the providers
+     *
+     * @return array An array of search objects
+     */
+    public function load(): array
+    {
+
+        $output = [];
+        foreach ($this->providers as $provider){
+            $output = array_merge($output, $provider->load());
+        }
+        return $output;
+    }
+}
