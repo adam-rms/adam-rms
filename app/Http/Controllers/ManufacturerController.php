0b7d9f8ebdc4feff6f061e14aee4cca878583b73
James Bithell <Jbithell@users.noreply.github.com>
2022-01-23 09:02:57 +0000

    Add manufacturer controller

---
+++ app/Http/Controllers/ManufacturerController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Manufacturer;
+use Illuminate\Http\Request;
+
+class ManufacturerController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Manufacturer  $manufacturer
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Manufacturer $manufacturer)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Manufacturer  $manufacturer
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Manufacturer $manufacturer)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Manufacturer  $manufacturer
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Manufacturer $manufacturer)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Manufacturer  $manufacturer
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Manufacturer $manufacturer)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('user/{id}', 'UserController@showProfile');
+
+Route::controller('manufacturer', 'ManufacturerController');
