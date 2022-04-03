9e1ddf0cc769f8d5bc48f65ce6ea0a582fc9e96a
Robert Watts <11817698+Robert-Watts@users.noreply.github.com>
2022-04-03 15:50:04 +0100

    Instance search (#301)
    * Search CMS
    
    * Search Projects
    
    * Add Search Clients and add comments
    
    * Add Search Locations
    
    * Fix incorrect use of array filter
    
    * Add User interface for search
    
    * Drop requirement for html2text as the internet seems somewhat nervous about it
    
    * Go to filtered page for clients and locations
    
    * Fix composer from merge
    
    * Include archived projects in search results
    
    @Robert-Watts  not sure what you think about this?
    
    * Throw some text in for locations & clients
    
    * Round time display
    
    * Spelling
    
    Co-Authored-By: Robert Watts <11817698+Robert-Watts@users.noreply.github.com>
    
    * Add user search
    
    * Guide a user to also search assets
    
    * Move asset search button
    
    * Remove search icon
    
    * Revert "Remove search icon"
    
    This reverts commit 4d66eb56bb4f59f44d5972ebe8f7b76618d49836.
    
    Co-authored-by: James Bithell <Jbithell@users.noreply.github.com>

---
+++ html/admin/api/search/search.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../apiHead.php';
+require_once __DIR__ . '/../../../common/libs/Search/Search.php';
+
+// Get the GET variables
+$term = $_GET['term'] ? $_GET['term'] : "";
+$offset = is_numeric($_GET['offset']) ? intval($_GET['offset']) : 0;
+$limit = is_numeric($_GET['limit']) ? intval($_GET['limit']) : 20;
+
+
+try {
+    //Run the search
+    $search = new Search();
+    $results = $search->search($term, $limit, $offset);
+    finish(true, null, $results);
+}
+catch (ParseError $e) {
+    finish(false, ["Not Authenticated"]);
+}
+catch (ValueError $e) {
+    finish(false, ["message"=>"Business not found"]);
+}
+catch (Exception $e) {
+    throw $e;
+}
---
+++ html/common/libs/Search/SearchProjects.php
@@ -0,0 +1,69 @@
+<?php
+require_once __DIR__ . '/BaseSearch.php';
+
+class SearchProjects extends BaseSearch
+{
+    /**
+     * Load the projects that are accessible
+     *
+     * @return array An array of search objects
+     */
+    public function load(): array
+    {
+        // The user cannot access projects
+        if (!$this->AUTH->instancePermissionCheck(20)) {
+            return [];
+        }
+
+        // Get the projects
+        $this->DBLIB->where("projects.instances_id", $this->AUTH->data['instance']['instances_id']);
+        $this->DBLIB->where("projects.projects_deleted", 0);
+        $this->DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
+        $this->DBLIB->join("users", "projects.projects_manager=users.users_userid",);
+        $projects = $this->DBLIB->get("projects", null, ["projects_id", "projects_archived", "projects_name", "projects_description", "clients_name", "projects_dates_deliver_start", "projects_dates_deliver_end", "projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_manager as projects_manager_id", "concat(users_name1, ' ', users_name2) AS projects_manager"]);
+
+        // Generate the array and return the function
+        return array_map(function ($project) {
+            // Create a list of all set project dates
+            $dates = array_unique(array_merge(
+                ($project['projects_dates_deliver_start'] ? [strtotime($project['projects_dates_deliver_start'])] : []),
+                ($project['projects_dates_deliver_end'] ? [strtotime($project['projects_dates_deliver_end'])] : []),
+                ($project['projects_dates_use_start'] ? [strtotime($project['projects_dates_use_start'])] : []),
+                ($project['projects_dates_use_end'] ? [strtotime($project['projects_dates_use_end'])] : [])
+            ));
+
+            // Convert the set project dates into strings (to be able to search)
+            $readable_dates = [];
+            foreach ($dates as $date) {
+                array_push($readable_dates, date("l jS \of F Y h:i:s A", $date));
+                array_push($readable_dates, date("Y-m-d H:i:s", $date));
+            }
+
+            // Generate the except for the project
+            $except = "The project is called '" . $project['projects_name'] . "', and is being project managed by " . $project['projects_manager'] . ".";
+            if ($project['projects_dates_deliver_start'] and $project['projects_dates_deliver_end']) {
+                $except .= " The project runs from " . date("Y-m-d H:i:s", strtotime($project['projects_dates_deliver_start'])) . " to " . date("Y-m-d H:i:s", strtotime($project['projects_dates_deliver_end'])) . ".";
+            }
+            if ($project['projects_dates_use_start'] and $project['projects_dates_use_end']) {
+                $except .= " The assets are in use from " . date("Y-m-d H:i:s", strtotime($project['projects_dates_use_start'])) . " to " . date("Y-m-d H:i:s", strtotime($project['projects_dates_use_end'])) . ".";
+            }
+
+            // Return the data
+            return [
+                'type' => "project",
+                'searchable' => array_merge([
+                    $project['projects_name'],
+                    $project['projects_description'],
+                    $project['projects_manager'],
+                    $project['projects_status'],
+                    $project['clients_name'],
+                ], $readable_dates),
+                'title' => $project['projects_name'],
+                'except' => $except,
+                'data' => $project
+            ];
+
+        }, $projects);
+
+    }
+}
