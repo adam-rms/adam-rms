784404bca773974ef0e1822e9f0cf4b77f463f42
James Bithell <Jbithell@users.noreply.github.com>
2022-04-30 15:11:46 +0100

    Set env variables before migrations

--- railwayStart.sh
+++ railwayStart.sh
@@ -2,6 +2,15 @@
 
 echo "Starting Container"
 
+echo "Setting Environment Vars"
+export bCMS__ROOTURL="https://$RAILWAY_STATIC_URL"
+export bCMS__DB_HOSTNAME="$MYSQLHOST"
+export bCMS__DB_DATABASE="$MYSQLDATABASE"
+export bCMS__DB_USERNAME="$MYSQLUSER"
+export bCMS__DB_PASSWORD="$MYSQLPASSWORD"
+export bCMS__DB_PORT="$MYSQLPORT"
+echo "Vars set"
+
 echo "Running Migrations"
 cd /var/www/
 php vendor/bin/phinx migrate -e production
@@ -13,15 +22,6 @@ then
   php vendor/bin/phinx seed:run
   echo "Seed Run"
 fi
-
-echo "Setting Environment Vars"
-export bCMS__ROOTURL="https://$RAILWAY_STATIC_URL"
-export bCMS__DB_HOSTNAME="$MYSQLHOST"
-export bCMS__DB_DATABASE="$MYSQLDATABASE"
-export bCMS__DB_USERNAME="$MYSQLUSER"
-export bCMS__DB_PASSWORD="$MYSQLPASSWORD"
-export bCMS__DB_PORT="$MYSQLPORT"
-echo "Vars set"
 
 echo "Booting Apache"
 /usr/sbin/apache2 -D FOREGROUND
