a844ffd85d87b62055b1627ff52c9bf9d23931a5
James Bithell <Jbithell@users.noreply.github.com>
2022-04-30 14:22:40 +0100

    Update migrate.sh

--- migrate.sh
+++ migrate.sh
@@ -1,11 +1,17 @@
 #!/bin/sh
 
 echo "Starting Container"
-echo "$RAILWAY_ENVIRONMENT"
 
 echo "Running Migrations"
 cd /var/www/
 php vendor/bin/phinx migrate -e production
 echo "Migrations Run - Apache to takeover now"
 
+if [ "$RAILWAY_ENVIRONMENT" != "production" ]
+then
+  echo "Running Seed"
+  php vendor/bin/phinx seed:run
+fi
+
+echo "Booting Apache"
 /usr/sbin/apache2 -D FOREGROUND
