64a3fa047e88f5153b496769547aa2868fa11887
John Cherry <github@johncherry.me>
2022-05-15 18:41:13 +0100

    Add Descriptions

--- html/admin/api/instances/export/getColumns.php
+++ html/admin/api/instances/export/getColumns.php
@@ -12,36 +12,35 @@ if (!isset($_POST['table_name'])) finish(false, ["code" => "PARAM-ERROR", "messa
  * information that either is AdamRMS specific, or should not be exportable from a system.
  */
 
-//columns: tablename => [columnName]
+//columns: tablename => [name => Printable Name, value =>columnName, description => simple description for column ]
 $columns = [
     "assets" => [
-        "assets_id", 
-        "assets_tag", 
-        "assets_notes", 
-        "assets_dayRate", 
-        "assets_weekRate", 
-        "assets_value", 
-        "assets_mass", 
-        "asset_definableFields_1",
-        "asset_definableFields_2",
-        "asset_definableFields_3",
-        "asset_definableFields_4",
-        "asset_definableFields_5",
-        "asset_definableFields_6",
-        "asset_definableFields_7",
-        "asset_definableFields_8",
-        "asset_definableFields_9",
-        "asset_definableFields_10"
+        ["name" => "Asset Tag", "value" => "assets_tag",  "description" => "Unique Tag for this asset"],
+        ["name" => "Notes", "value" => "assets_notes",  "description" => "Asset Comments"],
+        ["name" => "Day Rate", "value" => "assets_dayRate",  "description" => "Hire charge for one day"],
+        ["name" => "Week Rate", "value" => "assets_weekRate",  "description" => "Hire charge for one week"],
+        ["name" => "Value", "value" => "assets_value",  "description" => "Purchase cost"],
+        ["name" => "Mass", "value" => "assets_mass",  "description" => "Weight of asset"],
+        ["name" => "Definable Fields 1", "value" => "asset_definableFields_1", "description" => ""],
+        ["name" => "Definable Fields 2", "value" => "asset_definableFields_2", "description" => ""],
+        ["name" => "Definable Fields 3", "value" => "asset_definableFields_3", "description" => ""],
+        ["name" => "Definable Fields 4", "value" => "asset_definableFields_4", "description" => ""],
+        ["name" => "Definable Fields 5", "value" => "asset_definableFields_5", "description" => ""],
+        ["name" => "Definable Fields 6", "value" => "asset_definableFields_6", "description" => ""],
+        ["name" => "Definable Fields 7", "value" => "asset_definableFields_7", "description" => ""],
+        ["name" => "Definable Fields 8", "value" => "asset_definableFields_8", "description" => ""],
+        ["name" => "Definable Fields 9", "value" => "asset_definableFields_9", "description" => ""],
+        ["name" => "Definable Fields 10", "value" => "asset_definableFields_10", "description" => ""],
     ],
     "projects" => [
-        "projects_id",
-        "projects_name",
-        "projects_description",
-        "projects_dates_use_start",
-        "projects_dates_use_end",
-        "projects_dates_deliver_start",
-        "projects_dates_deliver_end",
-        "projects_invoiceNotes",
+        ["name" => "ID", "value" => "projects_id",  "description" => "Unique Tag for this asset"],
+        ["name" => "Name", "value" => "projects_name", "description" => "Project Name"],
+        ["name" => "Description", "value" => "projects_description", "description" => ""],
+        ["name" => "Event Start Date", "value" => "projects_dates_use_start", "description" => "Project event start date and time"],
+        ["name" => "Event End Date", "value" => "projects_dates_use_end", "description" => "Project event end date and time"],
+        ["name" => "Event Dispatch Date", "value" => "projects_dates_deliver_start", "description" => "Project asset dispatch date and time"],
+        ["name" => "Event Return Date", "value" => "projects_dates_deliver_end", "description" => "Project asset return date and time"],
+        ["name" => "Invoice Notes", "value" => "projects_invoiceNotes", "description" => "Notes for project invoice"],
     ]
 ];
 
