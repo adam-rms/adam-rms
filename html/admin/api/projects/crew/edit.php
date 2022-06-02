9f47c5266dd87918257b7ee73651ac8ef5fb4485
James Bithell <Jbithell@users.noreply.github.com>
2022-06-02 15:45:45 +0100

    Add event role to email and tweak use of "event" to correct term "project" (#361)

--- html/admin/api/projects/crew/edit.php
+++ html/admin/api/projects/crew/edit.php
@@ -13,7 +13,7 @@ if (!$assignment) finish(false);
 else {
     $bCMS->auditLog("EDIT-CREW", "crewAssignments", $assignment['crewAssignments_id'], $AUTH->data['users_userid'],null, $assignment['projects_id']);
     if ($assignment["users_userid"] and $_POST['crewAssignments_role'] != $assignment['crewAssignments_role']) { //Only email if the role name has changed, not just role comments
-        notify(20, $assignment["users_userid"], $AUTH->data['instance']['instances_id'], $AUTH->data['users_name1'] . " " . $AUTH->data['users_name2'] . " changed your role on the event " . $assignment['projects_name'] . " to be " . $bCMS->sanitizeString($_POST['crewAssignments_role']));
+        notify(20, $assignment["users_userid"], $AUTH->data['instance']['instances_id'], $AUTH->data['users_name1'] . " " . $AUTH->data['users_name2'] . " changed your role on the project " . $assignment['projects_name'] . " to be " . $bCMS->sanitizeString($_POST['crewAssignments_role']));
     }
     $DBLIB->where("crewAssignments_id", $assignment['crewAssignments_id']);
     if ($DBLIB->update("crewAssignments", ["crewAssignments_role" => $_POST['crewAssignments_role'], "crewAssignments_comment" => $_POST['crewAssignments_comment']])) finish(true);
