89d31410c45bfd22b9b0553a7b9422b5f23ec0e5
John Cherry <github@johncherry.me>
2022-07-05 20:52:13 +0100

    Correct subproject PM listing

--- html/admin/project/list.php
+++ html/admin/project/list.php
@@ -64,7 +64,7 @@ foreach ($projectlist as $project) {
     $DBLIB->orderBy("projects.projects_dates_use_start", "ASC");
     $DBLIB->orderBy("projects.projects_name", "ASC");
     $DBLIB->orderBy("projects.projects_created", "ASC");
-    $project['subProjects'] = $DBLIB->get("projects", null, ["projects_id", "projectsTypes.*","projects_archived", "projects_name", "clients_name", "projects_dates_deliver_start", "projects_dates_deliver_end","projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_manager", "projects_parent_project_id"]);
+    $project['subProjects'] = $DBLIB->get("projects", null, ["projects_id", "projectsTypes.*","projects_archived", "projects_name", "clients_name", "projects.clients_id", "projects_dates_deliver_start", "projects_dates_deliver_end","projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_manager", "users.users_name1", "users.users_name2", "users.users_email", "users.users_thumbnail"]);
 
     $PAGEDATA['PROJECTSLIST'][] = $project;
 }
