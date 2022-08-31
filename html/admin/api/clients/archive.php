0847da2ce3a186814c940d76bf53242c0ed151de
John Cherry <github@johncherry.me>
2022-08-31 15:13:55 +0100

    Correct argument names

--- html/admin/api/clients/archive.php
+++ html/admin/api/clients/archive.php
@@ -6,18 +6,18 @@
  * Archives a client
  *
  * Arguments:
- *  - client_id [int]: a client's id
+ *  - clients_id [int]: a client's id
  */
 
 require_once __DIR__ . '/../apiHeadSecure.php';
 
 if (!$AUTH->instancePermissionCheck(39)) die("404");
 
-if (!isset($_POST['client_id'])) die("404");
+if (!isset($_POST['clients_id'])) die("404");
 
 $DBLIB->where("clients.instances_id", $AUTH->data['instance']['instances_id']);
 $DBLIB->where("clients.clients_deleted", 0);
-$DBLIB->where("clients.clients_id", $_POST['client_id']);
+$DBLIB->where("clients.clients_id", $_POST['clients_id']);
 $client = $DBLIB->update("clients", ["clients.clients_archived" => 1]);
 
 if (!$client) finish(false);
--- html/admin/api/locations/archive.php
+++ html/admin/api/locations/archive.php
@@ -6,18 +6,18 @@
  * Archives a location
  *
  * Arguments:
- *  - location_id [int]: a location's id
+ *  - locations_id [int]: a location's id
  */
 
 require_once __DIR__ . '/../apiHeadSecure.php';
 
 if (!$AUTH->instancePermissionCheck(39)) die("404");
 
-if (!isset($_POST['location_id'])) die("404");
+if (!isset($_POST['locations_id'])) die("404");
 
 $DBLIB->where("locations.instances_id", $AUTH->data['instance']['instances_id']);
 $DBLIB->where("locations.locations_deleted", 0);
-$DBLIB->where("locations.locations_id", $_POST['location_id']);
+$DBLIB->where("locations.locations_id", $_POST['locations_id']);
 $client = $DBLIB->update("locations", ["locations.locations_archived" => 1]);
 
 if (!$client) finish(false);
