95d5547ca1255e4ea26353662b96a6a09ff82eec
John Cherry <github@johncherry.me>
2022-09-18 17:20:34 +0100

    Changes to app-oauth to allow V2 auth

--- html/admin/login/index.php
+++ html/admin/login/index.php
@@ -1,21 +1,27 @@
 <?php
 require_once __DIR__ . '/../common/head.php';
+
 use \Firebase\JWT\JWT;
+
 $PAGEDATA['pageConfig'] = ["TITLE" => "Login"];
 
 if (isset($_GET['app-oauth'])) {
 	$_SESSION['return'] = false;
-	$_SESSION['app-oauth'] = "com.bstudios.adamrms";
+	if (isset($_GET['returnHost'])) {
+		$_SESSION['app-oauth'] = 'https://' . $_GET['returnHost'] . '/';
+	} else {
+		$_SESSION['app-oauth'] = "com.bstudios.adamrms://";
+	}
 	if ($GLOBALS['AUTH']->login) {
 		$token = $GLOBALS['AUTH']->generateToken($AUTH->data['users_userid'], false, null, true, "App OAuth - already logged in");
 		$jwt = JWT::encode(array(
 			"iss" => $CONFIG['ROOTURL'],
 			"uid" => $AUTH->data['users_userid'],
 			"token" => $token,
-			"exp" => time()+21*24*60*60, //21 days token expiry
+			"exp" => time() + 21 * 24 * 60 * 60, //21 days token expiry
 			"iat" => time()
 		), $CONFIG['JWTKey']);
-		header("Location: " . $_SESSION['app-oauth'] . "://oauth_callback?token=" . $jwt);
+		header("Location: " . $_SESSION['app-oauth'] . "oauth_callback?token=" . $jwt);
 	}
 } elseif (isset($_GET['logout'])) {
 	$AUTH->logout();
@@ -57,23 +63,22 @@ elseif (isset($_GET['google'])) {
 		exit;
 	}
 
-	$DBLIB->where("users_oauth_googleid",$userProfile->identifier);
-	$user = $DBLIB->getOne("users",["users.users_suspended", "users.users_userid", "users.users_hash","users.users_emailVerified"]);
+	$DBLIB->where("users_oauth_googleid", $userProfile->identifier);
+	$user = $DBLIB->getOne("users", ["users.users_suspended", "users.users_userid", "users.users_hash", "users.users_emailVerified"]);
 	if ($user) {
 		if ($user['users_suspended'] != '0') {
 			$PAGEDATA['ERROR'] = "Sorry, your user account is suspended";
 			echo $TWIG->render('login/error.twig', $PAGEDATA);
 			exit;
-		}
-        elseif ($user['users_emailVerified'] != 1 and strlen($userProfile->emailVerified)<1) {
+		} elseif ($user['users_emailVerified'] != 1 and strlen($userProfile->emailVerified) < 1) {
 			$PAGEDATA['ERROR'] = "Please verify your email address using the link we sent you to login, or verify your email with Google to login";
 			echo $TWIG->render('login/error.twig', $PAGEDATA);
 			exit;
 		} else {
-			if ($user['users_emailVerified'] != 1 and strlen($userProfile->emailVerified)>0) {
+			if ($user['users_emailVerified'] != 1 and strlen($userProfile->emailVerified) > 0) {
 				// Update their email verification status
-				$DBLIB->where("users_userid",$user['users_userid']);
-				$DBLIB->update("users",["users_emailVerified" => 1]);
+				$DBLIB->where("users_userid", $user['users_userid']);
+				$DBLIB->update("users", ["users_emailVerified" => 1]);
 			}
 			//Log them in successfully - duplicated below for signup
 			if (!$_SESSION['return'] and isset($_SESSION['app-oauth'])) {
@@ -82,23 +87,25 @@ elseif (isset($_GET['google'])) {
 					"iss" => $CONFIG['ROOTURL'],
 					"uid" => $user['users_userid'],
 					"token" => $token,
-					"exp" => time()+21*24*60*60, //21 days token expiry
+					"exp" => time() + 21 * 24 * 60 * 60, //21 days token expiry
 					"iat" => time()
 				), $CONFIG['JWTKey']);
-				header("Location: " . $_SESSION['app-oauth'] . "://oauth_callback?token=" . $jwt);exit;
+				header("Location: " . $_SESSION['app-oauth'] . "://oauth_callback?token=" . $jwt);
+				exit;
 			} else {
-				$GLOBALS['AUTH']->generateToken($user['users_userid'], false,null, true, "Web - Google");
-				header("Location: " . (isset($_SESSION['return']) ? $_SESSION['return'] : $CONFIG['ROOTURL']));exit;
+				$GLOBALS['AUTH']->generateToken($user['users_userid'], false, null, true, "Web - Google");
+				header("Location: " . (isset($_SESSION['return']) ? $_SESSION['return'] : $CONFIG['ROOTURL']));
+				exit;
 			}
-        }
-	} elseif (strlen($userProfile->emailVerified)<1) {
+		}
+	} elseif (strlen($userProfile->emailVerified) < 1) {
 		$PAGEDATA['ERROR'] = "Please verify your email with Google to continue to login";
 		echo $TWIG->render('login/error.twig', $PAGEDATA);
 		exit;
 	} else {
 		//See if an email is found, but not linked to google. We don't want to auto-link them because its a good attack vector, so instead prompt a password login and then link in account settings.
-		$DBLIB->where("users_email",strtolower($userProfile->emailVerified));
-		$user = $DBLIB->getOne("users",["users.users_suspended", "users.users_userid", "users.users_hash","users.users_emailVerified"]);
+		$DBLIB->where("users_email", strtolower($userProfile->emailVerified));
+		$user = $DBLIB->getOne("users", ["users.users_suspended", "users.users_userid", "users.users_hash", "users.users_emailVerified"]);
 		if ($user) {
 			$PAGEDATA['ERROR'] = "An AdamRMS account associated with the email address you selected has been found. Please login again using your AdamRMS username & password to link your account to a Google Account in AdamRMS account settings";
 			echo $TWIG->render('login/error.twig', $PAGEDATA);
@@ -107,26 +114,26 @@ elseif (isset($_GET['google'])) {
 	}
 
 	//Okay we can't find them, so lets sign them up to an account
-	$username = preg_replace("/[^a-zA-Z0-9]+/", "", $userProfile->firstName.$userProfile->lastName);
+	$username = preg_replace("/[^a-zA-Z0-9]+/", "", $userProfile->firstName . $userProfile->lastName);
 	while ($AUTH->usernameTaken($username)) {
 		$username .= "1";
 	}
-	$data = Array (
+	$data = array(
 		'users_email' => strtolower($userProfile->emailVerified),
 		'users_emailVerified' => 1,
-		'users_oauth_googleid'=>$userProfile->identifier,
+		'users_oauth_googleid' => $userProfile->identifier,
 		'users_username' => $username,
 		'users_name1' => $userProfile->firstName,
 		'users_name2' => $userProfile->lastName,
 		'users_hash' => $CONFIG['nextHash']
 	);
-    $newUser = $DBLIB->insert("users", $data);
-    if (!$newUser) {
+	$newUser = $DBLIB->insert("users", $data);
+	if (!$newUser) {
 		$PAGEDATA['ERROR'] = "Sorry something went wrong trying to create a new user account";
 		echo $TWIG->render('login/error.twig', $PAGEDATA);
 		exit;
 	}
-	$bCMS->auditLog("INSERT", "users", json_encode($data), null,$newUser);
+	$bCMS->auditLog("INSERT", "users", json_encode($data), null, $newUser);
 	if (!$_SESSION['return'] and isset($_SESSION['app-oauth'])) {
 		//Duplicated in index.php
 		$token = $GLOBALS['AUTH']->generateToken($newUser, false, null, true, "App OAuth - Google");
@@ -134,14 +141,14 @@ elseif (isset($_GET['google'])) {
 			"iss" => $CONFIG['ROOTURL'],
 			"uid" => $newUser,
 			"token" => $token,
-			"exp" => time()+21*24*60*60, //21 days token expiry
+			"exp" => time() + 21 * 24 * 60 * 60, //21 days token expiry
 			"iat" => time()
 		), $CONFIG['JWTKey']);
-		header("Location: " . $_SESSION['app-oauth'] . "://oauth_callback?token=" . $jwt);exit;
+		header("Location: " . $_SESSION['app-oauth'] . "://oauth_callback?token=" . $jwt);
+		exit;
 	} else {
-		$GLOBALS['AUTH']->generateToken($newUser, false,null, true, "Web - Google");
-		header("Location: " . (isset($_SESSION['return']) ? $_SESSION['return'] : $CONFIG['ROOTURL']));exit;
+		$GLOBALS['AUTH']->generateToken($newUser, false, null, true, "Web - Google");
+		header("Location: " . (isset($_SESSION['return']) ? $_SESSION['return'] : $CONFIG['ROOTURL']));
+		exit;
 	}
-}
-else echo $TWIG->render('login/login.twig', $PAGEDATA);
-?>
+} else echo $TWIG->render('login/login.twig', $PAGEDATA);
