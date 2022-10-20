2b4eebfd5d9fd26d99014e85ad3baa3e4d8af700
James Bithell <Jbithell@users.noreply.github.com>
2022-10-20 17:25:39 +0100

    Move project view over to new logic

---
+++ html/admin/project/projectInvoice.php
@@ -0,0 +1,23 @@
+<?php
+require_once __DIR__ . '/../common/headSecure.php';
+if (!$AUTH->instancePermissionCheck(20) or !isset($_GET['id'])) die($TWIG->render('404.twig', $PAGEDATA));
+require_once __DIR__ . '/../api/projects/data.php'; //Where most of the data comes from
+
+$PAGEDATA['GET'] = $_GET;
+$PAGEDATA['GET']['generate'] = true;
+
+$isQuote = $_GET['quote'] == "true";
+$PAGEDATA['GET']['quote'] = $isQuote;
+$typeId = $isQuote ? 21 : 20;
+
+$PAGEDATA['GET']['draft'] = $_GET['draft'] == "true";
+
+$DBLIB->where("s3files_meta_type", $typeId);
+$DBLIB->where("instances_id",$AUTH->data['instance']['instances_id']);
+$DBLIB->where("s3files_meta_subType",$_GET['id']);
+$count = $DBLIB->getValue ("s3files", "count(*)");
+if ($count) $fileNumber = ($count+1);
+else $fileNumber = 1;
+$PAGEDATA['fileNumber'] = $fileNumber;
+
+echo $TWIG->render('project/pdf.twig', $PAGEDATA);
