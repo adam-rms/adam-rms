4cb0808e227ac4289a301547b19eb5d75d5d232a
James Bithell <Jbithell@users.noreply.github.com>
2022-12-23 15:52:31 +0000

    Update getIcons.php

--- html/admin/api/icons/getIcons.php
+++ html/admin/api/icons/getIcons.php
@@ -1,24 +1,14 @@
 <?php
 require_once __DIR__ . '/../apiHeadSecure.php';
 
-if (!isset($_POST['limit'])) $_POST['limit'] = 20;
-
 $icons = json_decode(file_get_contents(__DIR__ . '/icons.json'), true);
 
 if (isset($_POST['search'])) {
     $icons = array_filter($icons, function($icon) {
-        return strpos($icon['text'], $_POST['search']) !== false;
+        return strpos($icon['code'], $_POST['search']) !== false;
     });
 }
 
-$icons = array_slice($icons, 0, $_POST['limit']);
+$icons = array_slice($icons, 0, 20);
 
-//format for select2
-foreach ($icons as $key => &$value) {
-    $value['id'] = 'fas fa-'.$value['text'];
-}
-
-$return = new stdClass();
-$return->results = $icons;
-
-finish(true, null, $return);
+finish(true, null, $icons);
