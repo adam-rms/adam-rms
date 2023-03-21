d38fae9d6f3f142c3f2d96caf127cb399e5251af
John Cherry <github@johncherry.me>
2023-03-21 19:49:47 +0000

    Add Status to list endpoint

--- html/admin/api/projects/list.php
+++ html/admin/api/projects/list.php
@@ -11,13 +11,15 @@ $DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
 $DBLIB->orderBy("projects.projects_dates_deliver_start", "ASC");
 $DBLIB->orderBy("projects.projects_name", "ASC");
 $DBLIB->orderBy("projects.projects_created", "ASC");
-$projects = $DBLIB->get("projects", null, ["projects_id", "projects_archived", "projects_name", "clients_name", "projects_dates_deliver_start", "projects_dates_deliver_end","projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_manager"]);
+$projects = $DBLIB->get("projects", null, ["projects_id", "projects_archived", "projects_name", "clients_name", "projects_dates_deliver_start", "projects_dates_deliver_end", "projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_customProjectStatus", "projects_manager"]);
 foreach ($projects as $project) {
     $return[] = [
         "projects_id" => $project['projects_id'],
         "projects_name" => $project['projects_name'],
         "clients_name" => $project['clients_name'],
         "projects_manager" => $project['projects_manager'],
+        "projects_status" => $project['projects_status'],
+        "projects_customProjectStatus" => $project['projects_customProjectStatus'],
         "thisProjectManager" => ($AUTH->data['users_userid'] == $project['projects_manager'])
     ];
 }
