87554cb27e0c9cf9ffbe997b3e7b9e35b1ee2103
James Bithell <Jbithell@users.noreply.github.com>
2023-04-01 16:20:00 +0100

    Add migration for status table

---
+++ db/migrations/20230401130000_custom_project_status.php
@@ -0,0 +1,252 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+use Phinx\Db\Adapter\MysqlAdapter;
+
+final class CustomProjectStatus extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $originalStatuses = [
+            0 => [
+                "projectsStatuses_name" => "Added to RMS",
+                "projectsStatuses_description" => "Default",
+                "projectsStatuses_foregroundColour" => "#000000",
+                "projectsStatuses_backgroundColour" => "#F5F5F5",
+                "projectsStatuses_rank" => 0,
+                "projectsStatuses_assetsAvailable" => false,
+                "projectsStatuses_isCancelled" => false,
+                "projectsStatuses_class" => "info"
+            ],
+            1 => [
+                "projectsStatuses_name" => "Targeted",
+                "projectsStatuses_description" => "Being targeted as a lead",
+                "projectsStatuses_foregroundColour" => "#000000",
+                "projectsStatuses_backgroundColour" => "#F5F5F5",
+                "projectsStatuses_rank" => 1,
+                "projectsStatuses_assetsAvailable" => false,
+                "projectsStatuses_isCancelled" => false,
+                "projectsStatuses_class" => "info"
+            ],
+            2 => [
+                "projectsStatuses_name" => "Quote Sent",
+                "projectsStatuses_description" => "Waiting for client confirmation",
+                "projectsStatuses_foregroundColour" => "#000000",
+                "projectsStatuses_backgroundColour" => "#ffdd99",
+                "projectsStatuses_rank" => 2,
+                "projectsStatuses_assetsAvailable" => false,
+                "projectsStatuses_isCancelled" => false,
+                "projectsStatuses_class" => "warning"
+            ],
+            3 => [
+                "projectsStatuses_name" => "Confirmed",
+                "projectsStatuses_description" => "Booked in with client",
+                "projectsStatuses_foregroundColour" => "#ffffff",
+                "projectsStatuses_backgroundColour" => "#66ff66",
+                "projectsStatuses_rank" => 3,
+                "projectsStatuses_assetsAvailable" => false,
+                "projectsStatuses_isCancelled" => false,
+                "projectsStatuses_class" => "success"
+            ],
+            4 => [
+                "projectsStatuses_name" => "Prep",
+                "projectsStatuses_description" => "Being prepared for dispatch" ,
+                "projectsStatuses_foregroundColour" => "#000000",
+                "projectsStatuses_backgroundColour" => "#ffdd99",
+                "projectsStatuses_rank" => 4,
+                "projectsStatuses_assetsAvailable" => false,
+                "projectsStatuses_isCancelled" => false,
+                "projectsStatuses_class" => "success"
+            ],
+            5 => [
+                "projectsStatuses_name" => "Dispatched",
+                "projectsStatuses_description" => "Sent to client" ,
+                "projectsStatuses_foregroundColour" => "#ffffff",
+                "projectsStatuses_backgroundColour" => "#66ff66",
+                "projectsStatuses_rank" => 5,
+                "projectsStatuses_assetsAvailable" => false,
+                "projectsStatuses_isCancelled" => false,
+                "projectsStatuses_class" => "primary"
+            ],
+            6 => [
+                "projectsStatuses_name" => "Returned",
+                "projectsStatuses_description" => "Waiting to be checked in ",
+                "projectsStatuses_foregroundColour" => "#000000",
+                "projectsStatuses_backgroundColour" => "#ffdd99",
+                "projectsStatuses_rank" => 6,
+                "projectsStatuses_assetsAvailable" => false,
+                "projectsStatuses_isCancelled" => false,
+                "projectsStatuses_class" => "primary"
+            ],
+            7 => [
+                "projectsStatuses_name" => "Closed",
+                "projectsStatuses_description" => "Pending move to Archive",
+                "projectsStatuses_foregroundColour" => "#000000",
+                "projectsStatuses_backgroundColour" => "#F5F5F5",
+                "projectsStatuses_rank" => 7,
+                "projectsStatuses_assetsAvailable" => false,
+                "projectsStatuses_isCancelled" => false,
+                "projectsStatuses_class" => "secondary"
+            ],
+            8 => [
+                "projectsStatuses_name" => "Cancelled",
+                "projectsStatuses_description" => "Project Cancelled",
+                "projectsStatuses_foregroundColour" => "#000000",
+                "projectsStatuses_backgroundColour" => "#F5F5F5",
+                "projectsStatuses_rank" => 8,
+                "projectsStatuses_assetsAvailable" => true,
+                "projectsStatuses_isCancelled" => true,
+                "projectsStatuses_class" => "danger"
+            ],
+            9 => [
+                "projectsStatuses_name" => "Lead Lost",
+                "projectsStatuses_description" => "Project Cancelled",
+                "projectsStatuses_foregroundColour" => "#000000",
+                "projectsStatuses_backgroundColour" => "#F5F5F5",
+                "projectsStatuses_rank" => 9,
+                "projectsStatuses_assetsAvailable" => true,
+                "projectsStatuses_isCancelled" => true,
+                "projectsStatuses_class" => "danger"
+            ]
+        ];
+
+
+        $this->table('projectsStatuses', [
+                'id' => false,
+                'primary_key' => ['projectsStatuses_id'],
+                'engine' => 'InnoDB',
+                'encoding' => 'latin1',
+                'collation' => 'latin1_swedish_ci',
+                'comment' => '',
+                'row_format' => 'DYNAMIC',
+            ])
+            ->addColumn('projectsStatuses_id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'identity' => 'enable',
+            ])
+            ->addColumn('projectsStatuses_name', 'string', [
+                'null' => false,
+                'limit' => 200,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'projectsStatuses_id',
+            ])
+            ->addColumn('projectsStatuses_description', 'string', [
+                'null' => false,
+                'limit' => 9000,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'projectsStatuses_name',
+            ])
+            ->addColumn('projectsStatuses_fontAwesome', 'string', [
+                'null' => true,
+                'limit' => 100,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'projectsStatuses_name',
+            ])
+            ->addColumn('projectsStatuses_foregroundColour', 'string', [
+                'null' => false,
+                'limit' => 200,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'projectsStatuses_fontAwesome',
+            ])
+            ->addColumn('projectsStatuses_backgroundColour', 'string', [
+                'null' => false,
+                'limit' => 200,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'projectsStatuses_foregroundColour',
+            ])
+            ->addColumn('projectsStatuses_class', 'string', [
+                'null' => false,
+                'limit' => 200,
+                'collation' => 'latin1_swedish_ci',
+                'encoding' => 'latin1',
+                'after' => 'projectsStatuses_backgroundColour',
+            ])
+            ->addColumn('projectsStatuses_rank', 'integer', [
+                'null' => false,
+                'default' => '999',
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'projectsStatuses_fontAwesome',
+            ])
+            ->addColumn('projectsStatuses_assetsAvailable', 'boolean', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_TINY,
+                'after' => 'projectsStatuses_rank',
+            ])
+            ->addColumn('projectsStatuses_isCancelled', 'boolean', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_TINY,
+                'after' => 'projectsStatuses_assetsAvailable',
+            ])
+            ->addColumn('instances_id', 'integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR,
+                'after' => 'projectsStatusesGroups_id',
+            ])
+            ->addColumn('projectsStatuses_deleted', 'boolean', [
+                'null' => false,
+                'default' => '0',
+                'limit' => MysqlAdapter::INT_TINY,
+                'after' => 'instances_id',
+            ])
+            ->addIndex(['instances_id'], [
+                'name' => 'projectsStatuses_instances_instances_id_fk',
+                'unique' => false,
+            ])
+            ->addForeignKey('instances_id', 'instances', 'instances_id', [
+                'constraint' => 'projectsStatuses_instances_instances_id_fk',
+                'update' => 'CASCADE',
+                'delete' => 'CASCADE',
+            ])
+            ->create();
+
+        $this->table('projects')
+            ->changeColumn('projects_status','integer', [
+                'null' => false,
+                'limit' => MysqlAdapter::INT_REGULAR
+            ])
+            ->save();
+        
+        $instances = $this->fetchAll('SELECT instances_id FROM instances');   
+        foreach ($instances as $instance) {
+            foreach ($originalStatuses as $statusId => $status) {
+                $status['instances_id'] = $instance['instances_id'];
+                $this->table('projectsStatuses')->insert($status)->saveData();
+                $thisId = $this->getAdapter()->getConnection()->lastInsertId();
+                if (!$thisId) throw new Exception('Failed to insert status');
+                $this->execute('UPDATE projects SET projects_status = ' . intVal($thisId) . ' WHERE instances_id = ' . $instance['instances_id']. ' AND projects_status = ' . $statusId);
+            }
+        }
+
+        $this->table('projects')
+            ->renameColumn('projects_status', 'projectsStatuses_id')
+            ->save();
+
+        $this->table('projects')
+        ->addForeignKey('projectsStatuses_id', 'projectsStatuses', 'projectsStatuses_id', [
+            'constraint' => 'projects_projectsStatuses_id_projectsStatuses_id_fk',
+            'update' => 'CASCADE',
+            'delete' => 'CASCADE',
+        ])
+        ->save();
+    }
+}
