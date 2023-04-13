a5aaac7ebe622aa6ecdac3e14ac2a182726373a2
James Bithell <Jbithell@users.noreply.github.com>
2023-04-13 09:08:10 +0100

    Migrate calendar exports to new project statuses

--- html/admin/api/instances/calendar-export.php
+++ html/admin/api/instances/calendar-export.php
@@ -2,7 +2,7 @@
 require_once __DIR__ . '/../apiHead.php';
 header('Content-Type: text/calendar; charset=utf-8');
 header('Content-Disposition: attachment; filename="cal.ics"');
-error_reporting(0);
+error_reporting(0); 
 ini_set('display_errors', 0);
 
 $tz  = 'Europe/London';
@@ -22,10 +22,11 @@ $DBLIB->where("projects.instances_id", $PAGEDATA['instance']['instances_id']);
 $DBLIB->where("projects.projects_deleted", 0);
 $DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
 $DBLIB->join("locations","projects.locations_id=locations.locations_id","LEFT");
+$DBLIB->join("projectsStatuses", "projects.projectsStatuses_id=projectsStatuses.projectsStatuses_id", "LEFT");
 $DBLIB->join("users AS pmusers", "projects.projects_manager=pmusers.users_userid", "LEFT");
-$DBLIB->where("(projects.projects_status NOT IN (" . implode(",", $GLOBALS['STATUSES-AVAILABLE']) . "))");
+$DBLIB->where("projectsStatuses.projectsStatuses_isCancelled", 0);
 $DBLIB->orderBy("projects.projects_dates_use_start", "ASC");
-$iCalProjects = $DBLIB->get("projects", null, ["pmusers.users_name1 AS pm_name1", "pmusers.users_name2 AS pm_name2","projects.projects_id", "projects.projects_description", "projects.projects_dates_use_start", "projects.projects_dates_use_end", "projects.projects_name", "clients.clients_name", "projects.projects_status", "locations.locations_name","locations.locations_address"]);
+$iCalProjects = $DBLIB->get("projects", null, ["pmusers.users_name1 AS pm_name1", "pmusers.users_name2 AS pm_name2","projects.projects_id", "projects.projects_description", "projects.projects_dates_use_start", "projects.projects_dates_use_end", "projects.projects_name", "clients.clients_name", "projectsStatuses.projectsStatuses_name", "locations.locations_name","locations.locations_address"]);
 
 $vCalendar = new \Eluceo\iCal\Component\Calendar($CONFIG['ROOTURL']);
 
@@ -39,7 +40,7 @@ foreach ($iCalProjects as $event) {
         ->setSummary($event['projects_name'] . ($event['clients_name'] ? " (" . $event['clients_name'] . ")" : ""))
         ->setCategories(['events', 'AdamRMS'])
         ->setLocation($event['locations_name'] . "\n" . $event['locations_address'], $event['locations_name'] . "\n" . $event['locations_address'])
-        ->setDescription("Event Status: " . $STATUSES[$event['projects_status']]['name'] . "\n" . "Description: ". $event['projects_description'] . "\n" . "Project Manager: " . $event['pm_name1'] . " " . $event['pm_name2'])
+        ->setDescription("Event Status: " . $event['projectsStatuses_name'] . "\n" . "Description: ". $event['projects_description'] . "\n" . "Project Manager: " . $event['pm_name1'] . " " . $event['pm_name2'])
     ;
     $vCalendar->addComponent($vEvent);
 }
