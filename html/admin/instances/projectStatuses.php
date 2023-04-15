4d4acc758dada937cf36bb9e69d353c746c36231
James Bithell <Jbithell@users.noreply.github.com>
2023-04-15 19:39:39 +0100

    Add page

---
+++ html/admin/instances/projectStatuses.php
@@ -0,0 +1,25 @@
+<?php
+require_once __DIR__ . '/../common/headSecure.php';
+
+$PAGEDATA['pageConfig'] = ["TITLE" => "Project Statuses", "BREADCRUMB" => false];
+
+if (!$AUTH->instancePermissionCheck("PROJECTS:PROJECT_STATUSES:VIEW")) die($TWIG->render('404.twig', $PAGEDATA));
+
+if (isset($_GET['q'])) $PAGEDATA['search'] = $bCMS->sanitizeString($_GET['q']);
+else $PAGEDATA['search'] = null;
+
+$DBLIB->orderBy("assetCategoriesGroups.assetCategoriesGroups_order", "ASC");
+$DBLIB->orderBy("assetCategories.assetCategories_rank", "ASC");
+if (strlen($PAGEDATA['search']) > 0) {
+    //Search
+    $DBLIB->where("(
+		assetCategories.assetCategories_name LIKE '%" . $bCMS->sanitizeStringMYSQL($PAGEDATA['search']) . "%' 
+    )");
+}
+$DBLIB->where("(instances_id IS NULL OR instances_id = '" . $AUTH->data['instance']["instances_id"] . "')");
+$DBLIB->where("assetCategories_deleted",0);
+$DBLIB->join("assetCategoriesGroups", "assetCategoriesGroups.assetCategoriesGroups_id=assetCategories.assetCategoriesGroups_id", "LEFT");
+$PAGEDATA['categories'] = $DBLIB->get('assetCategories');
+
+echo $TWIG->render('instances/projectStatuses.twig', $PAGEDATA);
+?>
