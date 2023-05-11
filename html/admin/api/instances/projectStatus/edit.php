5940b6c7163420d52c79662ab2d808a2368e286c
John Cherry <github@johncherry.me>
2023-05-11 20:47:15 +0100

    Bugfixes

--- html/admin/api/instances/projectStatus/edit.php
+++ html/admin/api/instances/projectStatus/edit.php
@@ -5,7 +5,11 @@ if (!$AUTH->instancePermissionCheck("PROJECTS:PROJECT_STATUSES:EDIT")) die("404"
 
 $array = [];
 foreach ($_POST['formData'] as $item) {
-    $array[$item['name']] = $item['value'];
+    if ($item['name'] == "projectsStatuses_assetsReleased") {
+        $array[$item['name']] = $item['value'] == "on" ? 1 : 0;
+    } else {
+        $array[$item['name']] = $item['value'];
+    }
 }
 if (strlen($array['projectsStatuses_id']) <1) finish(false, ["code" => "PARAM-ERROR", "message"=> "No data for action"]);
 
