71bf7897c83e837d3c582a3bd0eed2b5a75d34c9
John Cherry <github@johncherry.me>
2023-06-08 09:51:45 +0100

    438 project endpoint subprojects (#476)
    * Update API to consider subprojects
    
    * Tweak subproject return

--- html/admin/api/projects/list.php
+++ html/admin/api/projects/list.php
@@ -11,14 +11,43 @@ $DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
 $DBLIB->orderBy("projects.projects_dates_deliver_start", "ASC");
 $DBLIB->orderBy("projects.projects_name", "ASC");
 $DBLIB->orderBy("projects.projects_created", "ASC");
+
+if (isset($_POST['subprojects'])) {
+    $DBLIB->where("projects.projects_parent_project_id IS NULL");
+}
+
 $projects = $DBLIB->get("projects", null, ["projects.projects_id", "projects.projects_name", "clients.clients_name", "projects.projects_manager"]);
+
 foreach ($projects as $project) {
+    $subprojectData = [];
+
+    if (isset($_POST['subprojects'])) {
+        $DBLIB->where("projects.projects_parent_project_id", $project['projects_id']);
+        $DBLIB->where("projects.projects_deleted", 0);
+        $DBLIB->where("projects.projects_archived", 0);
+        $DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
+        $DBLIB->orderBy("projects.projects_dates_deliver_start", "ASC");
+        $DBLIB->orderBy("projects.projects_name", "ASC");
+        $DBLIB->orderBy("projects.projects_created", "ASC");
+        $subprojects = $DBLIB->get("projects", null, ["projects_id", "projects_archived", "projects_name", "clients_name", "projects_dates_deliver_start", "projects_dates_deliver_end", "projects_dates_use_start", "projects_dates_use_end", "projects_status", "projects_manager"]);
+        foreach ($subprojects as $subproject) {
+            $subprojectData[] = [
+                "projects_id" => $subproject['projects_id'],
+                "projects_name" => $subproject['projects_name'],
+                "clients_name" => $subproject['clients_name'],
+                "projects_manager" => $subproject['projects_manager'],
+                "thisProjectManager" => ($AUTH->data['users_userid'] == $subproject['projects_manager'])
+            ];
+        }
+    }
+
     $return[] = [
         "projects_id" => $project['projects_id'],
         "projects_name" => $project['projects_name'],
         "clients_name" => $project['clients_name'],
         "projects_manager" => $project['projects_manager'],
-        "thisProjectManager" => ($AUTH->data['users_userid'] == $project['projects_manager'])
+        "thisProjectManager" => ($AUTH->data['users_userid'] == $project['projects_manager']),
+        "subprojects" => $subprojectData,
     ];
 }
 finish(true, null, $return);
