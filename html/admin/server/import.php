05616de2bdbccad49ef7215546da3b95ed16b2c1
John Cherry <github@johncherry.me>
2023-07-17 21:31:24 +0100

    Add import page

---
+++ html/admin/server/import.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/../common/headSecure.php';
+
+$PAGEDATA['pageConfig'] = ["TITLE" => "Instances", "BREADCRUMB" => false];
+
+if (!$AUTH->serverPermissionCheck("INSTANCES:IMPORT:ASSETS")) die($TWIG->render('404.twig', $PAGEDATA));
+
+$DBLIB->orderBy("instances.instances_id", "ASC");
+$DBLIB->where("instances.instances_deleted", 0);
+$instances = $DBLIB->get("instances", null, ["instances.instances_id", "instances.instances_name"]);
+$PAGEDATA['instances'] = $instances;
+
+echo $TWIG->render('server/import_asset.twig', $PAGEDATA);
