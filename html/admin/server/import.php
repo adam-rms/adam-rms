c50ab45ce2310051947d127814b3dc6acf52be29
James Bithell <Jbithell@users.noreply.github.com>
2023-09-10 17:08:57 +0100

    Show asset category IDs

--- html/admin/server/import.php
+++ html/admin/server/import.php
@@ -10,4 +10,11 @@ $DBLIB->where("instances.instances_deleted", 0);
 $instances = $DBLIB->get("instances", null, ["instances.instances_id", "instances.instances_name"]);
 $PAGEDATA['instances'] = $instances;
 
+$DBLIB->orderBy("assetCategories.instances_id", "ASC"); 
+$DBLIB->orderBy("assetCategories_rank", "ASC");
+$DBLIB->where("assetCategories_deleted",0);
+$DBLIB->join("assetCategoriesGroups", "assetCategoriesGroups.assetCategoriesGroups_id=assetCategories.assetCategoriesGroups_id", "LEFT");
+$DBLIB->join("instances", "assetCategories.instances_id=instances.instances_id", "LEFT");
+$PAGEDATA['categories'] = $DBLIB->get('assetCategories', null, ["assetCategoriesGroups.assetCategoriesGroups_name", "assetCategories.assetCategories_name", "assetCategories.assetCategories_id", "instances.instances_name"]);
+
 echo $TWIG->render('server/import_asset.twig', $PAGEDATA);
