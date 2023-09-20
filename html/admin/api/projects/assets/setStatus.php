5650a414efe71743fc22a15174c8a8411bf9769e
James Bithell <Jbithell@users.noreply.github.com>
2023-09-20 19:09:21 +0100

    Remove use of order, to fix bugs with multiple instances and not being able to assign to highest numbered cols

--- html/admin/api/projects/assets/setStatus.php
+++ html/admin/api/projects/assets/setStatus.php
@@ -3,22 +3,6 @@ require_once __DIR__ . '/../../apiHeadSecure.php';
 
 if (!$AUTH->instancePermissionCheck("PROJECTS:PROJECT_ASSETS:EDIT:ASSIGNMENT_STATUS") || !isset($_POST['assetsAssignments_status']) || !(isset($_POST['assetsAssignments_id']) || isset($_POST['projects_id']))) finish(false);
 
-// $_POST['status_is_order'] is used when the given "assetsAssignments_status" is an order value rather than a status id.
-
-if ($_POST['status_is_order']){
-    $DBLIB->where("assetsAssignmentsStatus_order", $_POST['assetsAssignments_status']);
-    $DBLIB->where("assetsAssignmentsStatus_deleted", 0);
-    $DBLIB->where("instances_id", $AUTH->data['instance']['instances_id']);
-    $status_id = $DBLIB->getValue("assetsAssignmentsStatus", "assetsAssignmentsStatus_id", 1);
-} else {
-    $DBLIB->where("assetsAssignmentsStatus_id", $_POST['assetsAssignments_status']);
-    $DBLIB->where("assetsAssignmentsStatus_deleted", 0);
-    $DBLIB->where("instances_id", $AUTH->data['instance']['instances_id']);
-    $status_id = $DBLIB->getValue("assetsAssignmentsStatus", "assetsAssignmentsStatus_id", 1);
-}
-
-if (!$status_id) finish(false, ["message" => "Status ID not found"]);
-
 if (isset($_POST['assetsAssignments_id'])){
     $DBLIB->where("assetsAssignments_id", $_POST['assetsAssignments_id'], (is_array($_POST['assetsAssignments_id'])? 'IN' : '='));
 } elseif (isset($_POST['projects_id'])) {
@@ -31,7 +15,7 @@ $DBLIB->where("assetsAssignments.assetsAssignments_deleted", 0);
 $DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
 $DBLIB->where("projects.projects_deleted", 0);
 $DBLIB->join("projects", "assetsAssignments.projects_id=projects.projects_id", "LEFT");
-$assignment = $DBLIB->update("assetsAssignments", ["assetsAssignmentsStatus_id" => $status_id ]);
+$assignment = $DBLIB->update("assetsAssignments", ["assetsAssignmentsStatus_id" => $_POST['assetsAssignments_status']]);
 
 if (!$assignment) finish(false);
 else finish(true);
