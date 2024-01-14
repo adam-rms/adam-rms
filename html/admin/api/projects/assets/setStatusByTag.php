ef6e966e9764b80758850766d4c01229c09460ef
James Bithell <Jbithell@users.noreply.github.com>
2024-01-14 10:47:12 +0000

    Support all instances at once, by searching for assigned assets instead of all assets

--- html/admin/api/projects/assets/setStatusByTag.php
+++ html/admin/api/projects/assets/setStatusByTag.php
@@ -3,26 +3,26 @@ require_once __DIR__ . '/../../apiHeadSecure.php';
 
 if (!$AUTH->instancePermissionCheck("PROJECTS:PROJECT_ASSETS:EDIT:ASSIGNMENT_STATUS") or !isset($_POST['projects_id']) or !isset($_POST['assetsAssignments_status']) or !isset($_POST['text']) or strlen($_POST['text']) < 1) finish(false, ["message" => "Missing required fields","code"=>"MISSINGFIELDS"]);
 
-$DBLIB->where("assets_deleted",0);
-$DBLIB->where("assets_tag", $_POST["text"]);
-$asset = $DBLIB->getone("assets",["assets.assets_id"]);
-if ($asset and $asset['assets_id'] != null) {
-    $DBLIB->where("assetsAssignmentsStatus_id", $_POST['assetsAssignments_status']);
-    $DBLIB->where("instances_id", $AUTH->data['instance']['instances_id']);
-    $status = $DBLIB->getone("assetsAssignmentsStatus",["assetsAssignmentsStatus_id"]);
-    if (!$status or $status['assetsAssignmentsStatus_id'] == null) finish(false, ["message" => "Status not found","code"=>"STATUSNOTFOUND"]);
+$DBLIB->where("assets.assets_deleted",0);
+$DBLIB->where("assets.assets_tag", $_POST["text"]);
+$DBLIB->where("projects.projects_id", $_POST['projects_id']);
+$DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->where("projects.projects_deleted", 0);
+$DBLIB->join("projects", "assetsAssignments.projects_id=projects.projects_id", "LEFT");
+$DBLIB->join("assets", "assetsAssignments.assets_id=assets.assets_id", "LEFT");
+$assignment = $DBLIB->getone("assetsAssignments",["assets.assets_id", "assetsAssignments.assetsAssignments_id", "assetsAssignments.assetsAssignmentsStatus_id", "assets.instances_id"]);
+if (!$assignment or $assignment['assets_id'] == null) finish(false, ["message" => "Asset not found","code"=>"NOTFOUND"]);
+if ($assignment['assetsAssignmentsStatus_id'] == $_POST['assetsAssignments_status']) finish(true, null, ["assets_id" => $assignment['assets_id']]); // No change
 
-    $DBLIB->where("assets_id", $asset['assets_id']);
-    $DBLIB->where("projects.projects_id", $_POST['projects_id']);
-    $DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
-    $DBLIB->where("projects.projects_deleted", 0);
-    $DBLIB->join("projects", "assetsAssignments.projects_id=projects.projects_id", "LEFT");
-    $DBLIB->update("assetsAssignments", ["assetsAssignmentsStatus_id" => $_POST['assetsAssignments_status']], 1);
+$DBLIB->where("assetsAssignmentsStatus_id", $_POST['assetsAssignments_status']);
+$DBLIB->where("instances_id", $assignment['instances_id']); // Use the instance of the asset
+$status = $DBLIB->getone("assetsAssignmentsStatus",["assetsAssignmentsStatus_id"]);
+if (!$status or $status['assetsAssignmentsStatus_id'] == null) finish(false, ["message" => "Status not found","code"=>"STATUSNOTFOUND"]);
 
-    if ($DBLIB->count != 1) {
-        finish(false, ["message" => "Asset not assigned to project","code"=>"NOTASSIGNED"]);
-    } else {
-        $bCMS->auditLog("EDIT-STATUS", "assetsAssignments", $_POST['assetsAssignments_status'], $AUTH->data['users_userid'],null, $assignment['projects_id']);
-        finish(true, null, ["assets_id" => $asset['assets_id']]);
-    }
-} else finish(false, ["message" => "Asset not found","code"=>"NOTFOUND"]);
+$DBLIB->where("assetsAssignments_id", $assignment['assetsAssignments_id']);
+$update = $DBLIB->update("assetsAssignments", ["assetsAssignmentsStatus_id" => $status['assetsAssignmentsStatus_id']], 1);
+if (!$update) finish(false, ["message" => "Asset not assigned to project","code"=>"NOTASSIGNED"]);
+else {
+    $bCMS->auditLog("EDIT-STATUS", "assetsAssignments", $assignment['assetsAssignments_id'] . " set from " . $assignment['assetsAssignmentsStatus_id'] . " to " . $status['assetsAssignmentsStatus_id'] . " by direct tag entry", $AUTH->data['users_userid'],null, $_POST['projects_id']);
+    finish(true, null, ["assets_id" => $assignment['assets_id']]);
+}
