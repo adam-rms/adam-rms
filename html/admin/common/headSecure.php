bdf3a0b7d94466aaf873636a0240f15b8c81607b
James Bithell <Jbithell@users.noreply.github.com>
2024-01-20 09:22:48 +0000

    Limit payload length for analytics events (#602)

--- html/admin/common/headSecure.php
+++ html/admin/common/headSecure.php
@@ -31,7 +31,7 @@ $DBLIB->insert("analyticsEvents", [
     "instances_id" => $AUTH->data['instance'] ?  $AUTH->data['instance']['instances_id'] : null,
     "analyticsEvents_path" => strtok($_SERVER["REQUEST_URI"], '?'),
     "analyticsEvents_action" => 'PAGE-REQUEST',
-    "analyticsEvents_payload" => count($_GET) > 0 ? json_encode($_GET) : null,
+    "analyticsEvents_payload" => count($_GET) > 0 ? (strlen(json_encode($_GET)) > 65535 ? null : json_encode($_GET)) : null,
 ]);
 
 // Create a set of instances that can be joined via the trusted domains route
