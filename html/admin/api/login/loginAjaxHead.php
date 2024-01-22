b27223c3d8b5a39876a6490e16a7d9fd4ea40d34
James Bithell <Jbithell@users.noreply.github.com>
2024-01-22 18:04:12 +0000

    Update loginAjaxHead.php

--- html/admin/api/login/loginAjaxHead.php
+++ html/admin/api/login/loginAjaxHead.php
@@ -6,11 +6,3 @@
  * Time: 10:19 AM
  */
 require_once __DIR__ . '/../apiHead.php';
-
-/** @OA\Get(
- *     path="/login/loginAjaxHead.php", 
- *     summary="Login Ajax Head", 
- *     operationId="loginAjaxHead", 
- *     tags={"authentication"}, 
- *     )
- */
