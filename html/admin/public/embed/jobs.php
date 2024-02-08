1b0a0a7904891cd1436b06cd1cb37152ed8503d4
Adam Skrzymowski <35405850+adamskrz@users.noreply.github.com>
2024-02-08 19:34:08 +0000

    Fix enabling public vacancies page (#616)

--- html/admin/public/embed/jobs.php
+++ html/admin/public/embed/jobs.php
@@ -1,6 +1,6 @@
 <?php
 require_once 'head.php';
-if (!isset($PAGEDATA['INSTANCE']['publicData']['enableAssets']) or !$PAGEDATA['INSTANCE']['publicData']['enableVacancies']) die("Disabled by your AdamRMS administrator");
+if (!isset($PAGEDATA['INSTANCE']['publicData']['enableVacancies']) or !$PAGEDATA['INSTANCE']['publicData']['enableVacancies']) die("Disabled by your AdamRMS administrator");
 
 date_default_timezone_set('Europe/London'); //So deadlines show in right timezone
 $PAGEDATA['pageConfig'] = ["TITLE" => "Crew Role Vacancies", "BREADCRUMB" => false];
