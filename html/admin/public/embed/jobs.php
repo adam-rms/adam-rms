d3a98dc52da560f7b1c01ccf33f8bfb27189d702
Adam Skrzymowski <35405850+adamskrz@users.noreply.github.com>
2024-02-08 18:11:59 +0000

    Add Client Name to Crew Vacancies embed (#567)

--- html/admin/public/embed/jobs.php
+++ html/admin/public/embed/jobs.php
@@ -10,6 +10,7 @@ $DBLIB->where("projectsVacantRoles.projectsVacantRoles_visibleToGroups IS NULL")
 $DBLIB->where("(projectsVacantRoles.projectsVacantRoles_deadline IS NULL OR projectsVacantRoles.projectsVacantRoles_deadline >= '" . date("Y-m-d H:i:s") . "')");
 $DBLIB->where("(projectsVacantRoles.projectsVacantRoles_slots > projectsVacantRoles.projectsVacantRoles_slotsFilled)");
 $DBLIB->join("projects","projectsVacantRoles.projects_id=projects.projects_id","LEFT");
+$DBLIB->join("clients", "projects.clients_id=clients.clients_id", "LEFT");
 $DBLIB->join("users", "projects.projects_manager=users.users_userid", "LEFT");
 $DBLIB->where("projects.instances_id", $PAGEDATA['INSTANCE']['instances_id']);
 $DBLIB->where("projects.projects_deleted", 0);
@@ -19,7 +20,7 @@ $DBLIB->orderBy("projects.projects_dates_use_start","ASC");
 $DBLIB->orderBy("projectsVacantRoles.projects_id","ASC");
 $DBLIB->orderBy("projectsVacantRoles.projectsVacantRoles_deadline","ASC");
 $DBLIB->orderBy("projectsVacantRoles.projectsVacantRoles_added","ASC");
-$roles = $DBLIB->get("projectsVacantRoles",null,["projectsVacantRoles.*","projects.*","users.users_userid", "users.users_name1", "users.users_name2", "users.users_email"]);
+$roles = $DBLIB->get("projectsVacantRoles",null,["projectsVacantRoles.*","projects.*", "clients.clients_name", "users.users_userid", "users.users_name1", "users.users_name2", "users.users_email"]);
 $PAGEDATA['roles'] = [];
 foreach($roles as $role) {
     $role['projectsVacantRoles_questions'] = json_decode($role['projectsVacantRoles_questions'],true);
