9b8f3fb634f01df4523df36820cb67f1426b979d
James Bithell <Jbithell@users.noreply.github.com>
2024-02-10 15:02:09 +0000

    Tidy business settings (#604)
    Co-authored-by: John Cherry <github@johncherry.me>

---
+++ html/admin/instances/configuration/logo.php
@@ -0,0 +1,9 @@
+<?php
+require_once __DIR__ . '/../../common/headSecure.php';
+
+$PAGEDATA['pageConfig'] = ["TITLE" => "Business Logo", "BREADCRUMB" => false];
+
+if (!$AUTH->instancePermissionCheck("BUSINESS:BUSINESS_SETTINGS:VIEW")) die($TWIG->render('404.twig', $PAGEDATA));
+
+echo $TWIG->render('instances/configuration/instances_configuration_logo.twig', $PAGEDATA);
+?>
