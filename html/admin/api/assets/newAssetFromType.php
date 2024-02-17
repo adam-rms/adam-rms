3d06d10c2a83d1f25806ce32bb0acf9dfab2082b
John Cherry <github@johncherry.me>
2024-02-17 16:07:44 +0000

    Generate barcode on asset creation

--- html/admin/api/assets/newAssetFromType.php
+++ html/admin/api/assets/newAssetFromType.php
@@ -27,7 +27,23 @@ if (isset($array['assets_tag']) and $array['assets_tag'] != null) {
 $result = $DBLIB->insert("assets", array_intersect_key( $array, array_flip( ['assets_tag','assetTypes_id','assets_notes','instances_id','asset_definableFields_1','asset_definableFields_2','asset_definableFields_3','asset_definableFields_4','asset_definableFields_5','asset_definableFields_6','asset_definableFields_7','asset_definableFields_8','asset_definableFields_9','asset_definableFields_10','assets_assetGroups'] ) ));
 
 if (!$result) finish(false, ["code" => "INSERT-FAIL", "message"=> "Could not insert asset"]);
-else finish(true, null, ["assets_id" => $result, "assets_tag" => $array['assets_tag'], "assetTypes_id" => $array['assetTypes_id']]);
+
+//Generate asset barcode
+
+$assetBarcodeData = [
+    "assetsBarcodes_value" => "A" . (is_numeric($asset['assets_tag']) ? $asset['assets_tag'] : $asset['assets_id']),
+    "assetsBarcodes_type" => "CODE_128",
+    "assets_id" => $asset['assets_id'],
+    "users_userid" => $AUTH->data['users_userid'],
+    "assetsBarcodes_added" => date("Y-m-d H:i:s")
+];
+while (checkDuplicate($assetBarcodeData["assetsBarcodes_value"],$assetBarcodeData["assetsBarcodes_type"])) {
+    $assetBarcodeData["assetsBarcodes_value"] = mt_rand(1000,99999);
+}
+$insert = $DBLIB->insert("assetsBarcodes", $assetBarcodeData);
+//We don't really mind if the insert fails, we can always generate another one later...
+
+finish(true, null, ["assets_id" => $result, "assets_tag" => $array['assets_tag'], "assetTypes_id" => $array['assetTypes_id']]);
 
 /** @OA\Post(
  *     path="/assets/newAssetFromType.php", 
