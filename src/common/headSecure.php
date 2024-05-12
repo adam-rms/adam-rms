6d992f632ff84643e7cccd232f7d1fcca2618701
James Bithell <Jbithell@users.noreply.github.com>
2024-05-12 10:15:19 +0100

    Add dedicated project creation page

--- src/common/headSecure.php
+++ src/common/headSecure.php
@@ -62,23 +62,6 @@ if ($CONFIG['LINKS_TERMSOFSERVICEURL'] and ($PAGEDATA['USERDATA']['users_termsAc
         $PAGEDATA['pageConfig'] = ["TITLE" => "Business Suspended", "BREADCRUMB" => false, "NOMENU" => true];
         die($TWIG->render('index_instanceBillingIssue.twig', $PAGEDATA));
     }
-    //Potential project types
-    $DBLIB->where("projectsTypes_deleted", 0);
-    $DBLIB->where("instances_id", $AUTH->data['instance']['instances_id']);
-    $DBLIB->orderBy("projectsTypes_name", "ASC");
-    $PAGEDATA['potentialProjectTypes'] = $DBLIB->get("projectsTypes");
-    //Potential project managers
-    $DBLIB->orderBy("users.users_name1", "ASC");
-    $DBLIB->orderBy("users.users_name2", "ASC");
-    $DBLIB->orderBy("users.users_created", "ASC");
-    $DBLIB->where("users_deleted", 0);
-    $DBLIB->join("userInstances", "users.users_userid=userInstances.users_userid", "LEFT");
-    $DBLIB->join("instancePositions", "userInstances.instancePositions_id=instancePositions.instancePositions_id", "LEFT");
-    $DBLIB->where("instances_id",  $AUTH->data['instance']['instances_id']);
-    $DBLIB->where("userInstances.userInstances_deleted",  0);
-    $DBLIB->where("(userInstances.userInstances_archived IS NULL OR userInstances.userInstances_archived >= '" . date('Y-m-d H:i:s') . "')");
-    $PAGEDATA['potentialProjectManagers'] = $DBLIB->get('users', null, ["users.users_name1", "users.users_name2", "users.users_userid"]);
-
     //get all projects
     $DBLIB->where("projects.instances_id", $AUTH->data['instance']['instances_id']);
     $DBLIB->where("projects.projects_deleted", 0);
--- src/project/index.php
+++ src/project/index.php
@@ -125,6 +125,25 @@ if ($AUTH->instancePermissionCheck("PROJECTS:EDIT:STATUS")) {
     $PAGEDATA['POSSIBLEPROJECTSTATUSES'] = $DBLIB->get("projectsStatuses", null, ["projectsStatuses_id", "projectsStatuses_name", "projectsStatuses_description", "projectsStatuses_assetsReleased"]);
 }
 
+//Edit options - Potential project types for changing project type
+$DBLIB->where("projectsTypes_deleted", 0);
+$DBLIB->where("instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->orderBy("projectsTypes_name", "ASC");
+$PAGEDATA['potentialProjectTypes'] = $DBLIB->get("projectsTypes");
+
+//Edit options - Potential project managers for changing manager
+$DBLIB->orderBy("users.users_name1", "ASC");
+$DBLIB->orderBy("users.users_name2", "ASC");
+$DBLIB->orderBy("users.users_created", "ASC");
+$DBLIB->where("users_deleted", 0);
+$DBLIB->join("userInstances", "users.users_userid=userInstances.users_userid", "LEFT");
+$DBLIB->join("instancePositions", "userInstances.instancePositions_id=instancePositions.instancePositions_id", "LEFT");
+$DBLIB->where("instances_id",  $AUTH->data['instance']['instances_id']);
+$DBLIB->where("userInstances.userInstances_deleted",  0);
+$DBLIB->where("(userInstances.userInstances_archived IS NULL OR userInstances.userInstances_archived >= '" . date('Y-m-d H:i:s') . "')");
+$PAGEDATA['potentialProjectManagers'] = $DBLIB->get('users', null, ["users.users_name1", "users.users_name2", "users.users_userid"]);
+
+
 //Edit Options - can create sub projects?
 $PAGEDATA['canCreateSubProjects'] = $bCMS->instanceHasProjectCapacity($AUTH->data['instance']['instances_id']);
 
