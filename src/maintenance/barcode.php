4f5d12a48bc49e75fd8cfac1ea96599d543d64b3
James Bithell <Jbithell@users.noreply.github.com>
2024-07-07 18:59:25 +0100

    Get scanning working

--- src/maintenance/barcode.php
+++ src/maintenance/barcode.php
@@ -3,6 +3,6 @@ require_once __DIR__ . '/../common/headSecure.php';
 
 if (!$AUTH->instancePermissionCheck("ASSETS:ASSET_BARCODES:VIEW")) die($TWIG->render('404.twig', $PAGEDATA));
 
-$PAGEDATA['pageConfig'] = ["TITLE" => "Scan Asset Barcodes", "BREADCRUMB" => true];
+$PAGEDATA['pageConfig'] = ["TITLE" => "Scan Asset Barcodes", "BREADCRUMB" => false];
 
 echo $TWIG->render('maintenance/barcode.twig', $PAGEDATA);
