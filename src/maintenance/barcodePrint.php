4412b370372b7182cdc6d66413d02b49b582f3e0
James Bithell <Jbithell@users.noreply.github.com>
2024-07-07 15:03:37 +0100

    Add QR code generator

--- src/maintenance/barcodePrint.php
+++ src/maintenance/barcodePrint.php
@@ -2,21 +2,22 @@
 require_once __DIR__ . '/../common/headSecure.php';
 if (!$AUTH->instancePermissionCheck("ASSETS:ASSET_BARCODES:VIEW") or !isset($_GET['ids'])) die($TWIG->render('404.twig', $PAGEDATA));
 
-$ids = explode(",",$_GET['ids']);
-$groups = explode(",",$_GET['groups']);
+$ids = explode(",", $_GET['ids']);
+$groups = explode(",", $_GET['groups']);
 $PAGEDATA['assets'] = [];
-function checkDuplicate($value,$type) {
+function checkDuplicate($value, $type)
+{
     global $DBLIB;
-    $DBLIB->where("assetsBarcodes_value",$value);
-    $DBLIB->where("assetsBarcodes_type",$type);
-    $result = $DBLIB->getone("assetsBarcodes",["assetsBarcodes_id"]);
+    $DBLIB->where("assetsBarcodes_value", $value);
+    $DBLIB->where("assetsBarcodes_type", $type);
+    $result = $DBLIB->getone("assetsBarcodes", ["assetsBarcodes_id"]);
     if ($result) return true;
     else return false;
 }
 foreach ($groups as $group) {
     if ($group == null) continue;
     $DBLIB->where("FIND_IN_SET(" . $bCMS->sanitizestring($group) . ", assets.assets_assetGroups)");
-    $DBLIB->where("assets_deleted",0);
+    $DBLIB->where("assets_deleted", 0);
     $groupAssets = $DBLIB->get("assets", null, ["assets_id"]);
     foreach ($groupAssets as $asset) {
         if ($asset) $ids[] = $asset['assets_id'];
@@ -28,25 +29,25 @@ foreach ($ids as $id) {
     $DBLIB->join("manufacturers", "manufacturers.manufacturers_id=assetTypes.manufacturers_id", "LEFT");
     $DBLIB->where("assets.assets_deleted", 0);
     $DBLIB->where("assets.instances_id", $AUTH->data['instance']['instances_id']);
-    $DBLIB->where("assets.assets_id",$id);
+    $DBLIB->where("assets.assets_id", $id);
     $DBLIB->where("(assets.assets_endDate IS NULL OR assets.assets_endDate >= CURRENT_TIMESTAMP())");
-    $asset = $DBLIB->getOne("assets", null, ["assets.assets_id", "assetTypes.assetTypes_definableFields", "assets.asset_definableFields_1", "assetTypes.assetTypes_name", "asset.assets_mass", "assetTypes.assetTypes_mass","assets.assets_tag", "manufacturers.manufacturers_name"]);
+    $asset = $DBLIB->getOne("assets", null, ["assets.assets_id", "assetTypes.assetTypes_definableFields", "assets.asset_definableFields_1", "assetTypes.assetTypes_name", "asset.assets_mass", "assetTypes.assetTypes_mass", "assets.assets_tag", "manufacturers.manufacturers_name"]);
     if ($asset) {
-        $DBLIB->orderBy("assetsBarcodes.assetsBarcodes_added","DESC");
-        $DBLIB->where("assetsBarcodes.assetsBarcodes_type","CODE_128");
-        $DBLIB->where("assetsBarcodes.assetsBarcodes_deleted",0);
-        $DBLIB->where("assetsBarcodes.assets_id",$asset['assets_id']);
+        $DBLIB->orderBy("assetsBarcodes.assetsBarcodes_added", "DESC");
+        $DBLIB->where("assetsBarcodes.assetsBarcodes_type", isset($_GET['barcodeType']) ? $_GET['barcodeType'] : "CODE_128");
+        $DBLIB->where("assetsBarcodes.assetsBarcodes_deleted", 0);
+        $DBLIB->where("assetsBarcodes.assets_id", $asset['assets_id']);
         $asset['barcode'] = $DBLIB->getone("assetsBarcodes");
         if (!$asset['barcode']) {
             $assetBarcodeData = [
                 "assetsBarcodes_value" => $asset['assets_tag'],
-                "assetsBarcodes_type" => "CODE_128",
+                "assetsBarcodes_type" => isset($_GET['barcodeType']) ? $_GET['barcodeType'] : "CODE_128",
                 "assets_id" => $asset['assets_id'],
                 "users_userid" => $AUTH->data['users_userid'],
                 "assetsBarcodes_added" => date("Y-m-d H:i:s")
             ];
-            while (checkDuplicate($assetBarcodeData["assetsBarcodes_value"],$assetBarcodeData["assetsBarcodes_type"])) {
-                $assetBarcodeData["assetsBarcodes_value"] = mt_rand(1000,999999);
+            while (checkDuplicate($assetBarcodeData["assetsBarcodes_value"], $assetBarcodeData["assetsBarcodes_type"])) {
+                $assetBarcodeData["assetsBarcodes_value"] = mt_rand(1000, 999999);
             }
             $insert = $DBLIB->insert("assetsBarcodes", $assetBarcodeData);
             if ($insert) {
@@ -55,24 +56,24 @@ foreach ($ids as $id) {
             } else continue; //Don't handle this asset
         }
 
-        $asset['assetTypes_definableFields'] = explode(",",$asset['assetTypes_definableFields']);
+        $asset['assetTypes_definableFields'] = explode(",", $asset['assetTypes_definableFields']);
         $asset['isAsset'] = true;
         $PAGEDATA['assets'][] = $asset;
     }
 }
 if ($_GET['blanks'] > 0) {
-    $count = $DBLIB->getValue("assetsBarcodes","COUNT(*)");
+    $count = $DBLIB->getValue("assetsBarcodes", "COUNT(*)");
     $count += 1; //Add one to it for the new set
-    foreach (range(1,$_GET['blanks'],1) as $asset) {
+    foreach (range(1, $_GET['blanks'], 1) as $asset) {
         $assetBarcodeData = [
-            "assetsBarcodes_value" => $count+$asset,
-            "assetsBarcodes_type" => "CODE_128",
+            "assetsBarcodes_value" => $count + $asset,
+            "assetsBarcodes_type" => isset($_GET['barcodeType']) ? $_GET['barcodeType'] : "CODE_128",
             "assets_id" => null,
             "users_userid" => $AUTH->data['users_userid'],
             "assetsBarcodes_added" => date("Y-m-d H:i:s")
         ];
-        while (checkDuplicate($assetBarcodeData["assetsBarcodes_value"],$assetBarcodeData["assetsBarcodes_type"])) {
-            $assetBarcodeData["assetsBarcodes_value"] = mt_rand(1000,99999);
+        while (checkDuplicate($assetBarcodeData["assetsBarcodes_value"], $assetBarcodeData["assetsBarcodes_type"])) {
+            $assetBarcodeData["assetsBarcodes_value"] = mt_rand(1000, 99999);
         }
         $asset = [];
         $insert = $DBLIB->insert("assetsBarcodes", $assetBarcodeData);
@@ -89,5 +90,8 @@ $PAGEDATA['GET'] = $_GET;
 if (!isset($_GET['instanceName'])) $PAGEDATA['GET']['instanceName'] = false;
 elseif ($_GET['instanceName'] == "Hide" or $_GET['instanceName'] == null) $PAGEDATA['GET']['instanceName'] = false;
 
-die($TWIG->render('maintenance/barcodePrint.twig', $PAGEDATA));
-?>
+if (isset($_GET['type']) and $_GET['type'] == "printout") {
+    die($TWIG->render('maintenance/barcodePrint.twig', $PAGEDATA));
+} else {
+    die($TWIG->render('maintenance/barcodePrint-table.twig', $PAGEDATA));
+}
