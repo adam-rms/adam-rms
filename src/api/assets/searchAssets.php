7d586172fcf5fbdac9a3f78da1f56d70a6d70ff8
James Bithell <Jbithell@users.noreply.github.com>
2024-07-07 19:43:25 +0100

    Limit search correctly

--- src/api/assets/searchAssets.php
+++ src/api/assets/searchAssets.php
@@ -1,8 +1,8 @@
 <?php
 require_once __DIR__ . '/../apiHeadSecure.php';
 
-$DBLIB->where("(assetTypes.instances_id IS NULL OR assetTypes.instances_id = '" . $AUTH->data['instance']['instances_id'] . "')");
-$DBLIB->join("assetTypes","assets.assetTypes_id=assetTypes.assetTypes_id", "LEFT");
+$DBLIB->where("assets.instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->join("assetTypes", "assets.assetTypes_id=assetTypes.assetTypes_id", "LEFT");
 $DBLIB->join("manufacturers", "manufacturers.manufacturers_id=assetTypes.manufacturers_id", "LEFT");
 $DBLIB->join("assetCategories", "assetCategories.assetCategories_id=assetTypes.assetCategories_id", "LEFT");
 $DBLIB->join("assetCategoriesGroups", "assetCategoriesGroups.assetCategoriesGroups_id=assetCategories.assetCategoriesGroups_id", "LEFT");
@@ -13,12 +13,12 @@ if (isset($_POST['term'])) {
         manufacturers_name LIKE '%" . $bCMS->sanitizeStringMYSQL($_POST['term']) . "%' OR
 		assetTypes_description LIKE '%" . $bCMS->sanitizeStringMYSQL($_POST['term']) . "%' OR
 		assets_notes LIKE '%" . $bCMS->sanitizeStringMYSQL($_POST['term']) . "%' OR
-		assets_tag LIKE '%" . $bCMS->sanitizeStringMYSQL($_POST['term']). "' OR
+		assets_tag LIKE '%" . $bCMS->sanitizeStringMYSQL($_POST['term']) . "' OR
         assetTypes_name LIKE '%" . $bCMS->sanitizeStringMYSQL($_POST['term']) . "%'
     )");
 } else $DBLIB->orderBy("assetTypes_name", "ASC");
 $assets = $DBLIB->get("assets", 15, ["assets.assets_id", "assets.assets_tag", "assetTypes.assetTypes_name", "assetTypes.assetTypes_id", "assetCategories.assetCategories_name", "assetCategoriesGroups.assetCategoriesGroups_name", "manufacturers.manufacturers_name"]);
-if (!$assets) finish(false, ["code" => "LIST-ASSETS-FAIL", "message"=> "Could not search"]);
+if (!$assets) finish(false, ["code" => "LIST-ASSETS-FAIL", "message" => "Could not search"]);
 $assetsReturn = [];
 foreach ($assets as $asset) {
     $asset['tag'] = $asset['assets_tag'];
