2d53b12598fa3f47ce8215cb63f740db72327542
James Bithell <Jbithell@users.noreply.github.com>
2024-07-09 19:55:32 +0100

    Combine into one query to reduce DB load

--- src/api/assets/export.php
+++ src/api/assets/export.php
@@ -26,24 +26,16 @@ use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 $DBLIB->orderBy("assetCategories.assetCategories_id", "ASC");
 $DBLIB->orderBy("assetTypes.assetTypes_name", "ASC");
+$DBLIB->where("assets.instances_id", $AUTH->data['instance']['instances_id']);
+$DBLIB->where("assets_deleted", 0);
+$DBLIB->where("(assets.assets_endDate IS NULL OR assets.assets_endDate >= CURRENT_TIMESTAMP())");
+$DBLIB->join("assetTypes", "assetTypes.assetTypes_id=assets.assetTypes_id", "LEFT");
 $DBLIB->join("manufacturers", "manufacturers.manufacturers_id=assetTypes.manufacturers_id", "LEFT");
-$DBLIB->where("((SELECT COUNT(*) FROM assets WHERE assetTypes.assetTypes_id=assets.assetTypes_id AND assets.instances_id = '" . $AUTH->data['instance']['instances_id'] . "' AND assets_deleted = 0) > 0)");
 $DBLIB->join("assetCategories", "assetCategories.assetCategories_id=assetTypes.assetCategories_id", "LEFT");
 $DBLIB->join("assetCategoriesGroups", "assetCategoriesGroups.assetCategoriesGroups_id=assetCategories.assetCategoriesGroups_id", "LEFT");
-$assets = $DBLIB->get('assetTypes', null, ["assetTypes.*", "manufacturers.manufacturers_name", "assetCategories.assetCategories_name", "assetCategoriesGroups.assetCategoriesGroups_name"]);
-$PAGEDATA['assets'] = [];
-$count = 0;
-foreach ($assets as $asset) {
-    $DBLIB->where("assets.instances_id", $AUTH->data['instance']['instances_id']);
-    $DBLIB->where("assets.assetTypes_id", $asset['assetTypes_id']);
-    $DBLIB->where("assets_deleted", 0);
-    $DBLIB->where("(assets.assets_endDate IS NULL OR assets.assets_endDate >= CURRENT_TIMESTAMP())");
-    $DBLIB->orderBy("assets.assets_tag", "ASC");
-    $asset['assets'] = $DBLIB->get("assets");
-    $count += count($asset['assets']);
-    $PAGEDATA['assets'][] = $asset;
-}
-
+$assets = $DBLIB->get('assets', null, [
+    "assets.assets_id", "assets.assets_tag", "assets.assets_mass", "assets.assets_dayRate", "assets.assets_weekRate", "assets.assets_value", "assets.assets_notes", "assets.asset_definableFields_1", "assets.asset_definableFields_2", "assets.asset_definableFields_3", "assets.asset_definableFields_4", "assets.asset_definableFields_5", "assets.asset_definableFields_6", "assets.asset_definableFields_7", "assets.asset_definableFields_8", "assets.asset_definableFields_9", "assets.asset_definableFields_10", "assetTypes.assetTypes_name", "assetTypes.assetTypes_mass", "assetTypes.assetTypes_dayRate", "assetTypes.assetTypes_weekRate", "assetTypes.assetTypes_value", "assetTypes_definableFields", "manufacturers.manufacturers_name", "assetCategories.assetCategories_name", "assetCategoriesGroups.assetCategoriesGroups_name"
+]);
 //Get first asset added to give a created date
 $DBLIB->where("assets.instances_id", $AUTH->data['instance']['instances_id']);
 $DBLIB->where("assets_deleted", 0);
@@ -65,33 +57,35 @@ for ($x = 1; $x < $sheet->getHighestRow(); $x++) {
     $sheet->removeRow($x);
 }
 $finalData = [];
-foreach ($PAGEDATA['assets'] as $assetType) {
-    $assetType['definableFields'] = explode(",", $assetType['assetTypes_definableFields']);
-    if (count($assetType['definableFields']) != 10) $assetType['definableFields'] = ["","","","","","","","","","",""];
-    foreach ($assetType['assets'] as $asset) {
-        $latestScan = assetLatestScan($asset['assets_id']);
-        if ($latestScan['locations_name']) $assetLocation = $latestScan['locations_name'];
-        else if ($latestScan['assetTypes_name']) $assetLocation = "Inside asset " . $latestScan['assetTypes_name']; //TODO improve this to mean something a bit more
-        else if ($latestScan['assetsBarcodes_customLocation']) $assetLocation = $latestScan['assetsBarcodes_customLocation'];
 
-        $spreadsheet->getActiveSheet()->insertNewRowBefore(1, 1);
-        $array = [
-            $asset['assets_tag'],
-            $assetType['assetCategoriesGroups_name'] . " - " . $assetType['assetCategories_name'],
-            $assetType['assetTypes_name'],
-            ($assetType['manufacturers_id'] != 1 ? $assetType['manufacturers_name'] : ""),
-            $asset['assets_mass'] !== null ?  $asset['assets_mass'] : $assetType['assetTypes_mass'],
-            $moneyFormatter->format(new Money($asset['assets_dayRate'] !== null ? $asset['assets_dayRate'] : $assetType['assetTypes_dayRate'], new Currency($AUTH->data['instance']['instances_config_currency']))),
-            $moneyFormatter->format(new Money($asset['assets_weekRate'] !== null ? $asset['assets_weekRate'] : $assetType['assetTypes_weekRate'], new Currency($AUTH->data['instance']['instances_config_currency']))),
-            $moneyFormatter->format(new Money($asset['assets_value'] !== null ? $asset['assets_value'] : $assetType['assetTypes_value'], new Currency($AUTH->data['instance']['instances_config_currency']))),
-            $assetLocation,
-            $asset['assets_notes']
-        ];
-        for ($x = 1; $x <= 10; $x++) {
-            array_push($array, $assetType['definableFields'][$x-1] . ($assetType['definableFields'][$x-1] != null ? ": ": ""),$asset['asset_definableFields_' . $x]);
-        }
-        $sheet->fromArray($array, NULL, 'A2');
+foreach ($assets as $asset) {
+    $asset['definableFields'] = explode(",", $asset['assetTypes_definableFields']);
+    if (count($asset['definableFields']) != 10) $asset['definableFields'] = ["", "", "", "", "", "", "", "", "", "", ""];
+
+    $latestScan = assetLatestScan($asset['assets_id']);
+
+    if ($latestScan['locations_name']) $assetLocation = $latestScan['locations_name'];
+    else if ($latestScan['assetTypes_name']) $assetLocation = "Inside asset " . $latestScan['assetTypes_name']; //TODO improve this to mean something a bit more
+    else if ($latestScan['assetsBarcodes_customLocation']) $assetLocation = $latestScan['assetsBarcodes_customLocation'];
+
+    $spreadsheet->getActiveSheet()->insertNewRowBefore(1, 1);
+
+    $array = [
+        $asset['assets_tag'],
+        $asset['assetCategoriesGroups_name'] . " - " . $asset['assetCategories_name'],
+        $asset['assetTypes_name'],
+        ($asset['manufacturers_id'] != 1 ? $asset['manufacturers_name'] : ""),
+        $asset['assets_mass'] !== null ?  $asset['assets_mass'] : $asset['assetTypes_mass'],
+        $moneyFormatter->format(new Money($asset['assets_dayRate'] !== null ? $asset['assets_dayRate'] : $asset['assetTypes_dayRate'], new Currency($AUTH->data['instance']['instances_config_currency']))),
+        $moneyFormatter->format(new Money($asset['assets_weekRate'] !== null ? $asset['assets_weekRate'] : $asset['assetTypes_weekRate'], new Currency($AUTH->data['instance']['instances_config_currency']))),
+        $moneyFormatter->format(new Money($asset['assets_value'] !== null ? $asset['assets_value'] : $asset['assetTypes_value'], new Currency($AUTH->data['instance']['instances_config_currency']))),
+        $assetLocation,
+        $asset['assets_notes']
+    ];
+    for ($x = 1; $x <= 10; $x++) {
+        array_push($array, $asset['definableFields'][$x - 1] . ($asset['definableFields'][$x - 1] != null ? ": " : ""), $asset['asset_definableFields_' . $x]);
     }
+    $sheet->fromArray($array, NULL, 'A2');
 }
 if (isset($_POST['csv'])) {
     $writer = new \PhpOffice\PhpSpreadsheet\Writer\Csv($spreadsheet);
@@ -100,30 +94,32 @@ if (isset($_POST['csv'])) {
     $writer->setLineEnding("\r\n");
     $writer->setSheetIndex(0);
     $writer->save("php://output");
+    $spreadsheet->disconnectWorksheets();
+    unset($spreadsheet);
 } elseif (isset($_POST['xlsx'])) {
     //Header
     $sheet->fromArray(["Asset Code", "Category", "Name", "Manufacturer", "Mass (kg)", "Day Rate", "Week Rate", "Value", "Location", "Notes", "Definable Fields"], NULL, 'A1');
     $sheet->mergeCells('J1:AC1');
-    $sheet->getStyle("A1:AC1")->getFont()->setBold( true );
+    $sheet->getStyle("A1:AC1")->getFont()->setBold(true);
     $sheet->freezePane('B2');
     //Number Formats
-    $sheet->getStyle('E2:E' . ($count+1))
+    $sheet->getStyle('E2:E' . (count($assets) + 1))
         ->getNumberFormat()
         ->setFormatCode('#,##0.000');
-    $sheet->getStyle('F2:F' . ($count+1))
+    $sheet->getStyle('F2:F' . (count($assets) + 1))
         ->getNumberFormat()
-        ->setFormatCode( '_-£* #,##0.00_-;-£* #,##0.00_-;_-£* "-"??_-;_-@_-');
-    $sheet->getStyle('G2:G' . ($count+1))
+        ->setFormatCode('_-£* #,##0.00_-;-£* #,##0.00_-;_-£* "-"??_-;_-@_-');
+    $sheet->getStyle('G2:G' . (count($assets) + 1))
         ->getNumberFormat()
-        ->setFormatCode( '_-£* #,##0.00_-;-£* #,##0.00_-;_-£* "-"??_-;_-@_-');
-    $sheet->getStyle('H2:H' . ($count+1))
+        ->setFormatCode('_-£* #,##0.00_-;-£* #,##0.00_-;_-£* "-"??_-;_-@_-');
+    $sheet->getStyle('H2:H' . (count($assets) + 1))
         ->getNumberFormat()
-        ->setFormatCode( '_-£* #,##0.00_-;-£* #,##0.00_-;_-£* "-"??_-;_-@_-');
-    $sheet->getStyle('J2:J' . ($count+1))
+        ->setFormatCode('_-£* #,##0.00_-;-£* #,##0.00_-;_-£* "-"??_-;_-@_-');
+    $sheet->getStyle('J2:J' . (count($assets) + 1))
         ->getAlignment()
         ->setWrapText(true);
     //Column Widths
-    foreach(range('A','Z') as $columnID) {
+    foreach (range('A', 'Z') as $columnID) {
         $sheet->getColumnDimension($columnID)
             ->setAutoSize(true);
     }
@@ -134,6 +130,8 @@ if (isset($_POST['csv'])) {
 
     $writer = new Xlsx($spreadsheet);
     $writer->save('php://output');
+    $spreadsheet->disconnectWorksheets();
+    unset($spreadsheet);
 } else die("404");
 
 /** @OA\Post(
