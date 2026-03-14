7fd86ef3030dd58ae028afa269838fe7294bdf6e
James Bithell <Jbithell@users.noreply.github.com>
2026-03-14 18:02:39 +0000

    Update 20260306130000_bigint_financial_columns.php

--- db/migrations/20260306130000_bigint_financial_columns.php
+++ db/migrations/20260306130000_bigint_financial_columns.php
@@ -9,7 +9,7 @@ final class BigintFinancialColumns extends AbstractMigration
      * Change all monetary/rate/value columns from INT to BIGINT to support
      * currencies with large unit values (e.g. HUF) without overflow.
      */
-    public function up(): void
+    public function change(): void
     {
         $this->execute("
             ALTER TABLE `assetTypes`
@@ -38,38 +38,6 @@ final class BigintFinancialColumns extends AbstractMigration
                 MODIFY COLUMN `projectsFinanceCache_paymentsReceived` BIGINT NULL DEFAULT NULL,
                 MODIFY COLUMN `projectsFinanceCache_grandTotal` BIGINT NULL DEFAULT NULL,
                 MODIFY COLUMN `projectsFinanceCache_value` BIGINT NULL DEFAULT NULL;
-        ");
-    }
-
-    public function down(): void
-    {
-        $this->execute("
-            ALTER TABLE `assetTypes`
-                MODIFY COLUMN `assetTypes_dayRate` INT NOT NULL DEFAULT 0,
-                MODIFY COLUMN `assetTypes_weekRate` INT NOT NULL DEFAULT 0,
-                MODIFY COLUMN `assetTypes_value` INT NOT NULL DEFAULT 0;
-
-            ALTER TABLE `assets`
-                MODIFY COLUMN `assets_dayRate` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `assets_weekRate` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `assets_value` INT NULL DEFAULT NULL;
-
-            ALTER TABLE `assetsAssignments`
-                MODIFY COLUMN `assetsAssignments_customPrice` INT NOT NULL DEFAULT 0;
-
-            ALTER TABLE `payments`
-                MODIFY COLUMN `payments_amount` INT NOT NULL DEFAULT 0;
-
-            ALTER TABLE `projectsFinanceCache`
-                MODIFY COLUMN `projectsFinanceCache_equipmentSubTotal` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `projectsFinanceCache_equiptmentDiscounts` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `projectsFinanceCache_equiptmentTotal` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `projectsFinanceCache_salesTotal` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `projectsFinanceCache_staffTotal` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `projectsFinanceCache_externalHiresTotal` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `projectsFinanceCache_paymentsReceived` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `projectsFinanceCache_grandTotal` INT NULL DEFAULT NULL,
-                MODIFY COLUMN `projectsFinanceCache_value` INT NULL DEFAULT NULL;
         ");
     }
 }
