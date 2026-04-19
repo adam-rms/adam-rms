f093414626d95b7a1a64600bb8bbdfdf145eb968
James Bithell <Jbithell@users.noreply.github.com>
2026-04-19 11:41:18 +0100

    Delete s3 files when a file is deleted

---
+++ db/migrations/20260419113000_s3files_do_not_delete_on_instance_deletion.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+use Phinx\Db\Adapter\MysqlAdapter;
+use Phinx\Migration\AbstractMigration;
+
+final class S3FilesDoNotDeleteOnInstanceDeletion extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $this
+            ->table('s3files')
+            ->changeColumn(
+                'instances_id',
+                'integer',
+                [
+                    'null' => true,
+                    'comment' => 'The ID of the instance this file is associated with. Nullable because files may be retained even after instance deletion.',
+                ]
+            )
+            ->dropForeignKey('instances_id')
+            ->addForeignKey(
+                'instances_id',
+                'instances',
+                'instances_id',
+                ['delete' => 'SET_NULL', 'update' => 'CASCADE']
+            )
+            ->update();
+    }
+}
